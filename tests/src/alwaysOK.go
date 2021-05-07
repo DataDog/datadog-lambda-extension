cb3dfadee21b6e6e6f80bf981e0e0a50fc7dc55b
maxday <maxime.david@datadoghq.com>
2021-05-07 10:13:39 -0400

    firstDraft

---
+++ tests/src/alwaysOK.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"context"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handleRequest(ctx context.Context, ev events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+	}, nil
+}
+
+func main() {
+	lambda.Start(handleRequest)
+}
+
