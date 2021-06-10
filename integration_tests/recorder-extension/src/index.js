d7867155611e768a5449c1af2bdffb5fabf695fd
Maxime David <maxime.david@datadoghq.com>
2021-06-10 11:53:12 -0400

    Add metrics and logs integration testing (#2)
    integration-tests

---
+++ integration_tests/recorder-extension/src/index.js
@@ -0,0 +1,119 @@
+#!/usr/bin/env node
+
+const express = require('express');
+const fetch = require('node-fetch');
+const bodyParser = require('body-parser');
+const protobuf = require('protobufjs');
+
+const BASE_URL = `http://${process.env.AWS_LAMBDA_RUNTIME_API}/2020-01-01/extension`;
+const SHUTDOWN_EVENT = 'SHUTDOWN';
+
+const handleShutdown = async () => {
+    console.log('SHUTDOWN received in recorder');
+    //making sure that the server won't be closed when the extension will send data
+    await new Promise(r => setTimeout(r, 1000));
+    console.log('SHUTDOWN end');
+}
+
+async function register() {
+    const res = await fetch(`${BASE_URL}/register`, {
+        method: 'post',
+        body: JSON.stringify({
+            'events': [
+                SHUTDOWN_EVENT
+            ],
+        }),
+        headers: {
+            'Content-Type': 'application/json',
+            'Lambda-Extension-Name': 'a_recorder',
+        }
+    });
+
+    if (!res.ok) {
+        console.error('register failed', await res.text());
+    }
+    return res.headers.get('lambda-extension-identifier');
+}
+
+async function next(extensionId) {
+    const res = await fetch(`${BASE_URL}/event/next`, {
+        method: 'get',
+        headers: {
+            'Content-Type': 'application/json',
+            'Lambda-Extension-Identifier': extensionId,
+        }
+    });
+
+    if (!res.ok) {
+        console.error('next failed', await res.text());
+        return null;
+    }
+
+    return await res.json();
+}
+
+(async function main() {
+
+    const app = express();
+    const options = {
+        inflate: true,
+        limit: '300kb',
+        type: 'application/x-protobuf'
+    };
+
+    app.use(bodyParser.raw(options));
+    app.use(bodyParser.json());
+
+    const extensionId = await register();
+
+    const port = 3333;
+
+    app.get('/*', (req, res) => {
+        console.log("GET", req.url);
+        res.sendStatus(200);
+    });
+
+    app.post('/api/beta/sketches*', async (req, res) => {
+        const root = await protobuf.load('/opt/extensions/src/agent_payload.proto');
+        const SketchPayload = root.lookupType('datadog.agentpayload.SketchPayload');
+        const obj = SketchPayload.decode(req.body);
+        for(let i = 0; i < obj.sketches.length; ++i) {
+            console.log("[sketch]", JSON.stringify(obj.sketches[i]));
+        }
+        res.sendStatus(200);
+    });
+
+
+    app.post('/v1/input', async (req, res) => {
+        if(JSON.stringify(req.body) !== '{}') { // to avoid printing empty logs due to the connectivity test
+            for(let i = 0; i < req.body.length; ++i) {
+                const logString = JSON.stringify(req.body[i]);
+                if(logString.indexOf("[sketch]") === -1 && logString.indexOf("[log]") === -1) { // avoid inception
+                    console.log("[log]", logString);
+                }
+            }
+        }
+        res.sendStatus(200);
+    });
+
+    app.post('/*', (req, res) => {
+        console.log("POST", req.url);
+        res.sendStatus(200);
+    });
+
+    app.listen(port);
+
+    process.on('SIGINT', async () => await handleShutdown());
+    process.on('SIGTERM', async () => await handleShutdown());
+
+    while (true) {
+        const event = await next(extensionId);
+        if(event.eventType === SHUTDOWN_EVENT) {
+            await handleShutdown();
+            break;
+        } else {
+            await handleShutdown();
+            throw new Error('Unexpected event');
+        }
+    }
+})();
---
+++ integration_tests/run.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+
+LOGS_WAIT_SECONDS=30
+
+set -e
+
+script_utc_start_time=$(date -u +"%Y%m%dT%H%M%S")
+
+cd "./integration_tests"
+
+#zip extension
+cd recorder-extension
+yarn install
+mkdir -p extensions
+cp -R src extensions
+cp -R node_modules extensions
+cp a_recorder extensions
+zip -rq ext.zip extensions -x ".*" -x "__MACOSX" -x "extensions/.*"
+cd ..
+
+if [ -z "$NODE_LAYER_VERSION" ]; then
+    echo "NODE_LAYER_VERSION not found "
+    exit 1
+fi
+
+# random 8-character ID to avoid collisions with other runs
+stage=$(xxd -l 4 -c 4 -p < /dev/random)
+
+# always remove the stacks before exiting, no matter what
+function remove_stack() {
+    echo "Removing stack for stage : ${stage}"
+    NODE_LAYER_VERSION=${NODE_LAYER_VERSION} \
+    serverless remove --stage ${stage} 
+}
+
+# making sure the remove_stack function will be called no matter what
+trap remove_stack EXIT
+
+# deploying the stack
+NODE_LAYER_VERSION=${NODE_LAYER_VERSION} \
+serverless deploy --stage ${stage}
+
+# invoking functions
+metric_function_names=("enhancedMetricTest" "noEnhancedMetricTest" "timeoutMetricTest")
+log_function_names=("logTest")
+
+
+all_functions=("${metric_function_names[@]}" "${log_function_names[@]}")
+
+set +e # Don't exit this script if an invocation fails or there's a diff
+
+for function_name in "${all_functions[@]}"; do
+    NODE_LAYER_VERSION=${NODE_LAYER_VERSION} \
+    serverless invoke --stage ${stage} -f ${function_name}
+    # two invocations are needed since enhanced metrics are computed with the REPORT log line (which is trigered at the end of the first invocation)
+    return_value=$(serverless invoke --stage ${stage} -f ${function_name})
+
+    # Compare new return value to snapshot
+    diff_output=$(echo "$return_value" | diff - "./snapshots/expectedInvocationResult")
+    if [ $? -eq 1 ] && [ "$function_name" != "timeoutMetricTest" ]; then
+        echo "Failed: Return value for $function_name does not match snapshot:"
+        echo "$diff_output"
+        mismatch_found=true
+    else
+        echo "Ok: Return value for $function_name matches snapshot"
+    fi
+done
+
+echo "Sleeping $LOGS_WAIT_SECONDS seconds to wait for logs to appear in CloudWatch..."
+sleep $LOGS_WAIT_SECONDS
+
+for function_name in "${all_functions[@]}"; do
+    echo "Fetching logs for ${function_name} on ${stage}"
+    retry_counter=0
+    while [ $retry_counter -lt 10 ]; do
+        raw_logs=$(NODE_LAYER_VERSION=${NODE_LAYER_VERSION} serverless logs --stage ${stage} -f $function_name --startTime $script_utc_start_time)
+        fetch_logs_exit_code=$?
+        if [ $fetch_logs_exit_code -eq 1 ]; then
+            echo "Retrying fetch logs for $function_name..."
+            retry_counter=$(($retry_counter + 1))
+            sleep 10
+            continue
+        fi
+        break
+    done
+
+    if [[ " ${metric_function_names[@]} " =~ " ${function_name} " ]]; then
+        # Replace invocation-specific data like timestamps and IDs with XXXX to normalize logs across executions
+        logs=$(
+            echo "$raw_logs" | \
+            grep "\[sketch\]" | \
+            perl -p -e "s/(ts\":\")[0-9]{10}/\1XXX/g" | \
+            perl -p -e "s/(min\":)[0-9\.]{2,20}/\1XXX/g" | \
+            perl -p -e "s/(max\":)[0-9\.]{2,20}/\1XXX/g" | \
+            perl -p -e "s/(cnt\":)[0-9\.]{2,20}/\1XXX/g" | \
+            perl -p -e "s/(avg\":)[0-9\.]{2,20}/\1XXX/g" | \
+            perl -p -e "s/(sum\":)[0-9\.]{2,20}/\1XXX/g" | \
+            perl -p -e "s/(k\":\[)[0-9\.]{1,20}/\1XXX/g" | \
+            perl -p -e "s/(datadog-nodev)[0-9]+\.[0-9]+\.[0-9]+/\1X\.X\.X/g" | \
+            perl -p -e "s/(datadog_lambda:v)[0-9]+\.[0-9]+\.[0-9]+/\1X\.X\.X/g" | \
+            perl -p -e "s/$stage/XXXXXX/g" | \
+            sort
+        )
+    elif [[ " ${log_function_names[@]} " =~ " ${function_name} " ]]; then
+        logs=$(
+            echo "$raw_logs" | \
+            grep "\[log\]" | \
+            perl -p -e "s/(timestamp\":)[0-9]{13}/\1XXX/g" | \
+            perl -p -e "s/(ddtags\":\")[a-zA-Z0-9\:\-,_]+/\1XXX/g" | \
+            perl -p -e "s/(\"REPORT |START |END |HTTP ).*/\1XXX\"}}/g" | \
+            perl -p -e "s/(request_id\":\")[a-zA-Z0-9\-,]+/\1XXX/g"| \
+            perl -p -e "s/$stage/XXXXXX/g" | \
+            perl -p -e "s/(\"message\":\").*(XXX LOG)/\1\2\3/g"
+        )
+    else #traces are not yet integration-tested
+        logs=$(
+            echo "$raw_logs"
+        )
+    fi
+
+    function_snapshot_path="./snapshots/${function_name}"
+
+    if [ ! -f $function_snapshot_path ]; then
+        # If no snapshot file exists yet, we create one
+        echo "Writing logs to $function_snapshot_path because no snapshot exists yet"
+        echo "$logs" >$function_snapshot_path
+    elif [ -n "$UPDATE_SNAPSHOTS" ]; then
+        # If $UPDATE_SNAPSHOTS is set to true write the new logs over the current snapshot
+        echo "Overwriting log snapshot for $function_snapshot_path"
+        echo "$logs" >$function_snapshot_path
+    else
+        # Compare new logs to snapshots
+        diff_output=$(echo "$logs" | diff - $function_snapshot_path)
+        if [ $? -eq 1 ]; then
+            echo "Failed: Mismatch found between new $function_name logs (first) and snapshot (second):"
+            echo "$diff_output"
+            mismatch_found=true
+        else
+            echo "Ok: New logs for $function_name match snapshot"
+        fi
+    fi
+
+done
+
+if [ "$mismatch_found" = true ]; then
+    echo "FAILURE: A mismatch between new data and a snapshot was found and printed above."
+    exit 1
+fi
+
+echo "SUCCESS: No difference found between snapshots and new return values or logs"
