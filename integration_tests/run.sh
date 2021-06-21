17414e7febf0898e00ef525aa71e490774d423fe
maxday <maxime.david@datadoghq.com>
2021-06-21 18:18:13 -0400

    automatic layer version detection

--- integration_tests/run.sh
+++ integration_tests/run.sh
@@ -14,14 +14,27 @@ cd recorder-extension
     zip -rq ext.zip extensions/* -x ".*" -x "__MACOSX" -x "extensions/.*"
 cd ..
 
+#get the latest layer vesion number
+function getLatestLayerVersion() {
+    layerName=$1
+    lastVersion=$(aws lambda list-layer-versions --layer-name $layerName --region sa-east-1 | jq -r ".LayerVersions | .[0] |  .Version")
+    if [ lastVersion == "null" ]; then
+        exit 1
+    else
+        echo $lastVersion
+    fi
+}
+
 if [ -z "$NODE_LAYER_VERSION" ]; then
-    echo "NODE_LAYER_VERSION not found "
-    exit 1
+   echo "NODE_LAYER_VERSION not found, getting the latest one"
+   NODE_LAYER_VERSION=$(getLatestLayerVersion "Datadog-Node14-x")
+   echo "NODE_LAYER_VERSION set to : $NODE_LAYER_VERSION"
 fi
 
 if [ -z "$PYTHON_LAYER_VERSION" ]; then
-    echo "PYTHON_LAYER_VERSION not found "
-    exit 1
+   echo "PYTHON_LAYER_VERSION not found, getting the latest one"
+   PYTHON_LAYER_VERSION=$(getLatestLayerVersion "Datadog-Python38")
+   echo "PYTHON_LAYER_VERSION set to : $PYTHON_LAYER_VERSION"
 fi
 
 # random 8-character ID to avoid collisions with other runs
