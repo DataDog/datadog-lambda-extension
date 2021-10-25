8bc4c6458f6b3c1f4e1f141c5c4a0b4324a0a8ca
maxday <maxime.david@datadoghq.com>
2021-10-25 14:42:14 -0400

    comments

--- scripts/build_binary_and_layer_dockerized.sh
+++ scripts/build_binary_and_layer_dockerized.sh
@@ -5,6 +5,11 @@
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2021 Datadog, Inc.
 
+# Usage examples : 
+# ARCHITECTURE=amd64 VERSION=100 ./scripts/build_binary_and_layer_dockerized.sh
+# or 
+# VERSION=100 ./scripts/build_binary_and_layer_dockerized.sh
+
 if [ -z "$VERSION" ]; then
     echo "Extension version not specified"
     echo ""
@@ -61,7 +66,7 @@ elif [ $ARCHITECTURE == "arm64" ]; then
     echo "Building for arm64 only"
     docker_build_zip arm64
 else
-    echo "No valid ARCHITECTURE variable found (allowed values are : arn64, amd64), building for both amd64 and arn64"
+    echo "No valid ARCHITECTURE variable found (allowed values are : arm64, amd64), building for both amd64 and arm64"
     docker_build_zip amd64
     docker_build_zip arm64
 fi
