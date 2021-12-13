09f1b64362d078ab10971cca0bacd1752eaaad9b
Nick Hinsch <nick.hinsch@datadoghq.com>
2021-12-13 15:10:54 -0500

    Fix lowercasing

--- scripts/boot.sh
+++ scripts/boot.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 args=("$@")
+
+# lowercase DD_LOG_LEVEL
+DD_LOG_LEVEL=$(echo "$DD_LOG_LEVEL" | tr '[:upper:]' '[:lower:]')
+
 if [ "$DD_EXPERIMENTAL_ENABLE_PROXY" == "true" ]
 then
-  if [ grep -ix "$DD_LOG_LEVEL" <<< "debug" ]
+  if [ "$DD_LOG_LEVEL" == "debug" ]
   then
     echo "[bootstrap] DD_EXPERIMENTAL_ENABLE_PROXY is true"
     echo "[bootstrap] original AWS_LAMBDA_RUNTIME_API value is $AWS_LAMBDA_RUNTIME_API"
@@ -10,7 +14,7 @@ then
 
   export AWS_LAMBDA_RUNTIME_API="127.0.0.1:9000"
 
-  if [ grep -ix "$DD_LOG_LEVEL" <<< "debug" ]
+  if [ "$DD_LOG_LEVEL" == "debug" ]
   then
     echo "[bootstrap] rerouting AWS_LAMBDA_RUNTIME_API to $AWS_LAMBDA_RUNTIME_API"
   fi
