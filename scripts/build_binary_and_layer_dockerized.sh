d6b030470e6639cf350f225ebce5979ded58579f
Jordan Storms <jordan.storms@datadoghq.com>
2022-02-09 12:58:14 -0500

    first commit

--- scripts/build_binary_and_layer_dockerized.sh
+++ scripts/build_binary_and_layer_dockerized.sh
@@ -5,9 +5,9 @@
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2021 Datadog, Inc.
 
-# Usage examples : 
-# ARCHITECTURE=amd64 VERSION=100 ./scripts/build_binary_and_layer_dockerized.sh
-# or 
+# Usage examples :
+# ARCHITECTURE=amd64 VERSION=100 RACE_DETECTION_ENABLED=true ./scripts/build_binary_and_layer_dockerized.sh
+# or
 # VERSION=100 ./scripts/build_binary_and_layer_dockerized.sh
 
 if [ -z "$VERSION" ]; then
@@ -20,7 +20,7 @@ fi
 AGENT_PATH="../datadog-agent"
 
 # Move into the root directory, so this script can be called from any directory
-SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 ROOT_DIR=$SCRIPTS_DIR/..
 cd $ROOT_DIR
 
@@ -28,7 +28,7 @@ EXTENSION_DIR=".layers"
 TARGET_DIR=$(pwd)/$EXTENSION_DIR
 
 # Make sure the folder does not exist
-rm -rf $EXTENSION_DIR 2> /dev/null
+rm -rf $EXTENSION_DIR 2>/dev/null
 
 mkdir -p $EXTENSION_DIR
 
@@ -44,19 +44,24 @@ cd $AGENT_PATH/..
 tar --exclude=.git -czf $ROOT_DIR/scripts/.src/datadog-agent.tgz datadog-agent
 cd $ROOT_DIR
 
+# Make sure to use the right build file if we want race detection enabled
+BUILD_FILE=Dockerfile.build
+if [ "$RACE_DETECTION_ENABLED" = "true" ]; then
+    BUILD_FILE=Dockerfile.race.build
+fi
+
 function docker_build_zip {
     arch=$1
 
     docker buildx build --platform linux/${arch} \
         -t datadog/build-lambda-extension-${arch}:$VERSION \
-        -f ./scripts/Dockerfile.build \
+        -f ./scripts/$BUILD_FILE \
         --build-arg EXTENSION_VERSION="${VERSION}" . \
         --load
     dockerId=$(docker create datadog/build-lambda-extension-${arch}:$VERSION)
     docker cp $dockerId:/datadog_extension.zip $TARGET_DIR/datadog_extension-${arch}.zip
     unzip $TARGET_DIR/datadog_extension-${arch}.zip -d $TARGET_DIR/datadog_extension-${arch}
 }
-
 
 if [ "$ARCHITECTURE" == "amd64" ]; then
     echo "Building for amd64 only"
