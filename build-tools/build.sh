9a6858b1f8f7533853395642f9fcb9b514925130
maxday <maxime.david@datadoghq.com>
2022-11-08 08:55:02 -0500

    add binary

--- build-tools/build.sh
+++ build-tools/build.sh
@@ -1,3 +1,5 @@
 docker build . --progress=plain -t datadog/build-tools
 dockerId=$(docker create datadog/build-tools)
 docker cp $dockerId:/build_tools.zip .
+unzip build_tools.zip
+rm build_tools.zip
