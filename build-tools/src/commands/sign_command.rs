8b0a0e8ab7540e851328705ce46d578bf7e3a730
maxday <maxime.david@datadoghq.com>
2022-11-23 16:15:29 -0500

    format

--- build-tools/src/commands/sign_command.rs
+++ build-tools/src/commands/sign_command.rs
@@ -1,9 +1,14 @@
 use aws_sdk_lambda::types::ByteStream;
 use aws_sdk_s3 as s3;
 use aws_sdk_signer as signer;
+use core::time;
 use rand::{distributions::Alphanumeric, Rng};
-use signer::model::{Destination, S3Destination, S3Source, Source};
-use std::{io::Result, path::Path};
+use signer::model::{Destination, S3Destination, S3Source, SigningStatus, Source};
+use std::{
+    io::{Error, ErrorKind, Result},
+    path::Path,
+    thread,
+};
 use structopt::StructOpt;
 
 const BUCKET_NAME: &str = "dd-lambda-signing-bucket-sandbox";
@@ -21,12 +26,9 @@ pub async fn sign(args: &SignOptions) -> Result<()> {
     let s3_client = s3::Client::new(&config);
     let signer_client = signer::Client::new(&config);
     let key = build_s3_key();
-    upload_object(args, &key, &s3_client)
-        .await
-        .expect("could not upload the layer");
-    sign_object(&key, &signer_client)
-        .await
-        .expect("could not sign layer");
+    upload_object(args, &key, &s3_client).await?;
+    let job_id = sign_object(&key, &signer_client).await?;
+    verify(&job_id, &signer_client).await?;
     Ok(())
 }
 
@@ -58,8 +60,36 @@ async fn sign_object(key: &str, signer_client: &signer::Client) -> Result<String
         .expect("coud not start the signing job");
     match result.job_id() {
         Some(job_id) => Ok(String::from(job_id)),
-        None => panic!("could not find the job id"),
+        None => Err(Error::new(ErrorKind::NotFound, "could not find the job id")),
     }
+}
+
+fn is_job_completed(status: Option<&SigningStatus>) -> Result<bool> {
+    match status {
+        Some(SigningStatus::InProgress) => Ok(false),
+        Some(SigningStatus::Succeeded) => Ok(true),
+        _ => Err(Error::new(ErrorKind::InvalidData, "job has failed")),
+    }
+}
+
+async fn verify(job_id: &str, signer_client: &signer::Client) -> Result<()> {
+    let result = signer_client
+        .describe_signing_job()
+        .job_id(job_id)
+        .send()
+        .await
+        .expect("could not verify the job id");
+
+    let delay = time::Duration::from_secs(15);
+
+    for _ in 0..5 {
+        let result = is_job_completed(result.status())?;
+        if result {
+            return Ok(());
+        }
+        thread::sleep(delay);
+    }
+    Err(Error::new(ErrorKind::TimedOut, "the signing job timeouts"))
 }
 
 fn random_string() -> String {
