642a0751ad7dcb4b2695f8619869b50bc849fbb4
maxday <maxime.david@datadoghq.com>
2022-11-23 18:10:49 -0500

    clippy

--- build-tools/src/commands/sign_command.rs
+++ build-tools/src/commands/sign_command.rs
@@ -1,9 +1,9 @@
-use aws_sdk_lambda::types::{ByteStream, SdkError};
+use aws_sdk_lambda::types::{ByteStream};
 use aws_sdk_s3 as s3;
 use aws_sdk_signer as signer;
 use core::time;
 use rand::{distributions::Alphanumeric, Rng};
-use s3::{error::DeleteObjectError, output::DeleteObjectOutput};
+
 use signer::model::{Destination, S3Destination, S3Source, SigningStatus, Source};
 use std::{
     fs::File,
