0777620cdb3e6a6d87b342ff1556776ded050446
maxday <maxime.david@datadoghq.com>
2022-11-23 18:14:12 -0500

    fix

--- build-tools/build.sh
+++ build-tools/build.sh
@@ -1,4 +1,4 @@
-docker buildx build --platform linux/amd64 --push -t datadog/build-tools .
+docker buildx build --platform linux/amd64 -t datadog/build-tools .
 dockerId=$(docker create datadog/build-tools)
 docker cp $dockerId:/build_tools.zip .
 rm -rf bin
