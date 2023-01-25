aee66fee37c329e270cca51fdd5d5aca4b8c0146
Rey Abolofia <rey.abolofia@datadoghq.com>
2023-01-25 08:51:28 -0800

    Reset permissions for testing.

--- scripts/publish_layers.sh
+++ scripts/publish_layers.sh
@@ -125,18 +125,8 @@ publish_layer() {
     fi
     permission=$(aws lambda add-layer-version-permission --layer-name $layer \
         --version-number $version_nbr \
-        --statement-id "release-otel-$version_nbr-1" \
-        --action lambda:GetLayerVersion --principal "977805900156" \
-        --region $region)
-    permission=$(aws lambda add-layer-version-permission --layer-name $layer \
-        --version-number $version_nbr \
-        --statement-id "release-otel-$version_nbr-2" \
-        --action lambda:GetLayerVersion --principal "147553070075" \
-        --region $region)
-    permission=$(aws lambda add-layer-version-permission --layer-name $layer \
-        --version-number $version_nbr \
-        --statement-id "release-otel-$version_nbr-3" \
-        --action lambda:GetLayerVersion --principal "412554278814" \
+        --statement-id "release-$version_nbr" \
+        --action lambda:GetLayerVersion --principal "*" \
         --region $region)
 
     echo $version_nbr
