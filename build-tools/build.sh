035c38b4b06f0b9b5c06513b68616c0d29262d55
Maxime David <maxime.david@datadoghq.com>
2023-03-07 11:35:20 -0500

    ci

--- build-tools/build.sh
+++
@@ -1,6 +0,0 @@
-docker buildx build --platform linux/amd64 -t datadog/build-tools . --load
-dockerId=$(docker create --platform linux/amd64 datadog/build-tools)
-docker cp $dockerId:/build_tools.zip .
-rm -rf bin
-unzip build_tools.zip
-rm build_tools.zip
