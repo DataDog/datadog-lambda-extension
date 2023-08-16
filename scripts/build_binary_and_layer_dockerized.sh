800de86f20782427e9b90e6e472fde50532c1e7e
David Lee <david.lee@datadoghq.com>
2023-08-16 09:09:38 -0700

    cleanup

--- scripts/build_binary_and_layer_dockerized.sh
+++ scripts/build_binary_and_layer_dockerized.sh
@@ -86,19 +86,18 @@ if [ "$SERVERLESS_INIT" == "true" ]; then
     docker_build_zip amd64
     BUILD_FILE=Dockerfile.alpine.build
     docker_build_zip amd64 -alpine
-elif [ "$ALPINE" == "true" ]; then
-    echo "Building for both amd64 and arm64 on alpine"
-    BUILD_FILE=Dockerfile.alpine.build
-    docker_build_zip amd64 -alpine
-    docker_build_zip arm64 -alpine
 elif [ "$ARCHITECTURE" == "amd64" ]; then
     echo "Building for amd64 only"
     docker_build_zip amd64
+    docker_build_zip amd64 -alpine
 elif [ "$ARCHITECTURE" == "arm64" ]; then
     echo "Building for arm64 only"
     docker_build_zip arm64
+    docker_build_zip arm64 -alpine
 else
-    echo "Building for both amd64 and arm64"
+    echo "Building for both amd64 and arm64 (including alpine)"
     docker_build_zip amd64
     docker_build_zip arm64
+    docker_build_zip amd64 -alpine
+    docker_build_zip arm64 -alpine
 fi
