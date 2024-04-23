00e34a1fa9149bbf507f68bc3d80a78117a5ccdd
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2024-04-23 19:19:44 -0400

    parse telemetry byte data

--- bottlecap/src/telemetry/listener.rs
+++ bottlecap/src/telemetry/listener.rs
@@ -1,5 +1,9 @@
+use crate::telemetry::events::TelemetryEvent;
+use crate::events;
 use crate::TELEMETRY_PORT;
 
+use std::io::Write;
+use std::sync::mpsc::Sender;
 use std::{io::Read, net::{self, TcpStream}};
 
 use tracing::{debug, error};
@@ -9,7 +13,7 @@ pub struct TelemetryListener {
 }
 
 impl TelemetryListener {
-    pub fn run() -> TelemetryListener {
+    pub fn run(event_bus: Sender<events::Event>) -> TelemetryListener {
         let addr = format!("0.0.0.0:{}", TELEMETRY_PORT);
 
         let join_handle = std::thread::spawn(move || {
@@ -20,8 +24,9 @@ impl TelemetryListener {
                 for stream in listener.incoming() {
                     match stream {
                         Ok(stream) => {
-                            std::thread::spawn(|| {
-                                Self::handle_stream(stream); // does it make sense to spawn a new thread per connection?
+                            let cloned_event_bus = event_bus.clone();
+                            std::thread::spawn(move || {
+                                Self::handle_stream(stream, cloned_event_bus); // does it make sense to spawn a new thread per connection?
                             });
                             
                         },
@@ -35,16 +40,36 @@ impl TelemetryListener {
         TelemetryListener { join_handle }
     }
 
-    fn handle_stream(mut stream: TcpStream) {
+    fn handle_stream(mut stream: TcpStream, event_bus: Sender<events::Event>) {
+        debug!("Received a connection!");
         let mut buf = [0; 256 * 1024]; // Using the default limit from AWS
+        // TODO: Optimize this algorithm
         match stream.read(&mut buf) {
             Ok(_) => {
-                let message = std::str::from_utf8(&buf).expect("couldn't parse as string");
-                debug!("messsage {}", message);
+                // Not an optimal way to find where our message ends
+                // Could be improved using the content-lenght of the request
+                let end_index = buf.iter().position(|&r| r == 0).unwrap_or(buf.len());
+                // Is there a better way to do this? Sliding window?
+                let buf_str = std::str::from_utf8(&buf[..end_index]).expect("Couldn't parse as string");
+
+                // We only want the left part of the string, assuming it always
+                // contains data
+                let body = buf_str.split("\r\n\r\n").collect::<Vec<&str>>()[1];
+                
+                let telemetry_events: Vec<TelemetryEvent> = serde_json::from_str(body).expect("Couldn't parse telemetry event");
+                for event in telemetry_events {
+                    let _ = event_bus.send(events::Event::Telemetry(event));
+                }
             },
             Err(e) => {
                 error!("Error reading from stream: {}", e)
             }
+        }
+
+        let response = b"HTTP/1.1 200 OK";
+        match stream.write(response) {
+            Ok(_) => debug!("Acknowledge data receive"),
+            Err(e) => error!("Error sending response: {}", e),
         }
     }
 
