be7f95ba228b155b785ff604244d030dd16f6ec0
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2024-04-25 14:30:51 -0400

    optimize parsing algorithm

--- bottlecap/src/telemetry/listener.rs
+++ bottlecap/src/telemetry/listener.rs
@@ -1,76 +1,153 @@
-use crate::telemetry::events::TelemetryEvent;
 use crate::events;
+use crate::telemetry::events::TelemetryEvent;
 use crate::TELEMETRY_PORT;
 
-use std::io::Write;
+use std::collections::HashMap;
+use std::error::Error;
 use std::sync::mpsc::Sender;
-use std::{io::Read, net::{self, TcpStream}};
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
 
-use tracing::{debug, error};
+use tracing::debug;
+
+pub struct HttpRequestParser {
+    headers: HashMap<String, String>,
+    body: String,
+}
+
+const CR: u8 = b'\r';
+const LR: u8 = b'\n';
+
+impl HttpRequestParser {
+    pub fn from_buf(buf: &[u8]) -> Result<HttpRequestParser, Box<dyn Error>> {
+        let mut parser = HttpRequestParser {
+            headers: HashMap::new(),
+            body: String::new(),
+        };
+
+        let body_start_index = parser.parse_headers(&buf)?;
+        let _ = parser.parse_body(&buf, body_start_index);
+
+        Ok(parser)
+    }
+
+    fn parse_headers(&mut self, buf: &[u8]) -> Result<usize, Box<dyn Error>> {
+        let mut last_start = 0;
+        let mut i = 0;
+
+        // Ignore method, path, and protocol
+        while i + 1 < buf.len() {
+            if buf[i] == CR && buf[i + 1] == LR {
+                last_start = i + 2;
+                break;
+            }
+            i += 1;
+        }
+
+        // Parse headers
+        i = last_start;
+        while i < buf.len() {
+            if buf[i] == CR && buf[i + 1] == LR {
+                let header = std::str::from_utf8(&buf[last_start..i])?;
+                let mut header_parts = header.split(": ");
+                let key: &str = header_parts.next().unwrap();
+                let value = header_parts.next().unwrap();
+                self.headers
+                    .insert(key.to_string().to_lowercase(), value.to_string());
+
+                // Check if we reached the end of the headers
+                if i + 3 < buf.len() && buf[i + 2] == CR && buf[i + 3] == LR {
+                    last_start = i + 4;
+                    break;
+                }
+
+                last_start = i + 2;
+                i = last_start;
+                continue;
+            }
+            i += 1;
+        }
+
+        Ok(last_start)
+    }
+
+    fn parse_body(&mut self, buf: &[u8], start_index: usize) -> Result<(), Box<dyn Error>> {
+        let end_index = start_index
+            + self
+                .headers
+                .get("content-length")
+                .unwrap()
+                .parse::<usize>()?;
+
+        self.body = std::str::from_utf8(&buf[start_index..end_index])?.to_string();
+
+        Ok(())
+    }
+}
 
 pub struct TelemetryListener {
     join_handle: std::thread::JoinHandle<()>,
 }
 
 impl TelemetryListener {
-    pub fn run(event_bus: Sender<events::Event>) -> TelemetryListener {
+    pub fn run(event_bus: Sender<events::Event>) -> Result<TelemetryListener, Box<dyn Error>> {
         let addr = format!("0.0.0.0:{}", TELEMETRY_PORT);
+        let listener = TcpListener::bind(&addr)?;
+        let buf: [u8; 262144] = [0; 256 * 1024]; // Using the default limit from AWS
 
         let join_handle = std::thread::spawn(move || {
-            let listener = net::TcpListener::bind(&addr).expect("Couldn't bind to address");
             debug!("Initializating Telemetry Listener");
 
             loop {
                 for stream in listener.incoming() {
-                    match stream {
-                        Ok(stream) => {
-                            let cloned_event_bus = event_bus.clone();
-                            std::thread::spawn(move || {
-                                Self::handle_stream(stream, cloned_event_bus); // does it make sense to spawn a new thread per connection?
-                            });
-                            
-                        },
-                        Err(e) => {
-                            error!("Error accepting connection: {}", e)
-                        }
-                    }
+                    debug!("Received a Telemetry API connection");
+
+                    let cloned_event_bus = event_bus.clone();
+                    let stream = stream.unwrap();
+                    // TODO: revisit if it makes sense to spawn a new thread per connection.
+                    std::thread::spawn(move || {
+                        let r = Self::handle_stream(&stream, buf, cloned_event_bus);
+                        let _ = Self::acknowledge_request(stream, r);
+                    });
                 }
             }
         });
-        TelemetryListener { join_handle }
+
+        Ok(TelemetryListener { join_handle })
     }
 
-    fn handle_stream(mut stream: TcpStream, event_bus: Sender<events::Event>) {
-        debug!("Received a connection!");
-        let mut buf = [0; 256 * 1024]; // Using the default limit from AWS
-        // TODO: Optimize this algorithm
-        match stream.read(&mut buf) {
-            Ok(_) => {
-                // Not an optimal way to find where our message ends
-                // Could be improved using the content-lenght of the request
-                let end_index = buf.iter().position(|&r| r == 0).unwrap_or(buf.len());
-                // Is there a better way to do this? Sliding window?
-                let buf_str = std::str::from_utf8(&buf[..end_index]).expect("Couldn't parse as string");
+    fn handle_stream(
+        mut stream: &TcpStream,
+        mut buf: [u8; 262144],
+        event_bus: Sender<events::Event>,
+    ) -> Result<(), Box<dyn Error>> {
+        // Read into buffer
+        stream.read(&mut buf)?;
 
-                // We only want the left part of the string, assuming it always
-                // contains data
-                let body = buf_str.split("\r\n\r\n").collect::<Vec<&str>>()[1];
-                
-                let telemetry_events: Vec<TelemetryEvent> = serde_json::from_str(body).expect("Couldn't parse telemetry event");
-                for event in telemetry_events {
-                    let _ = event_bus.send(events::Event::Telemetry(event));
-                }
-            },
-            Err(e) => {
-                error!("Error reading from stream: {}", e)
+        let p = HttpRequestParser::from_buf(&buf)?;
+        let telemetry_events: Vec<TelemetryEvent> = serde_json::from_str(&p.body)?;
+        for event in telemetry_events {
+            let _ = event_bus.send(events::Event::Telemetry(event));
+        }
+
+        Ok(())
+    }
+
+    fn acknowledge_request(
+        mut stream: TcpStream,
+        request: Result<(), Box<dyn Error>>,
+    ) -> Result<(), Box<dyn Error>> {
+        match request {
+            Ok(_) => {
+                stream.write(b"HTTP/1.1 200 OK\r\n\r\n")?;
+            }
+            Err(_) => {
+                stream.write(b"HTTP/1.1 400 Bad Request\r\n\r\n")?;
             }
         }
-
-        let response = b"HTTP/1.1 200 OK\r\n\r\n";
-        match stream.write(response) {
-            Ok(_) => debug!("Acknowledge data receive"),
-            Err(e) => error!("Error sending response: {}", e),
-        }
+        Ok(())
     }
 
     pub fn shutdown(self) {
@@ -79,7 +156,7 @@ impl TelemetryListener {
                 debug!("Telemetry Listener thread has been shutdown");
             }
             Err(e) => {
-                error!("Error shutting down the Telemetry Listener thread: {:?}", e);
+                debug!("Error shutting down the Telemetry Listener thread: {:?}", e);
             }
         }
     }
