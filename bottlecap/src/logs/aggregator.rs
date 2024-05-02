db4a70a6ecd4e2377b424713106875cb9fe7d1f3
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2024-05-02 18:30:24 -0400

    add unit tests for `aggregator.rs`

--- bottlecap/src/logs/aggregator.rs
+++ bottlecap/src/logs/aggregator.rs
@@ -21,3 +21,59 @@ impl Aggregator {
         messages
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::logs::processor::{Lambda, LambdaMessage};
+
+    use super::*;
+
+    #[test]
+    fn add() {
+        let mut aggregator = Aggregator::new();
+        let log = IntakeLog {
+            message: LambdaMessage {
+                message: "test".to_string(),
+                lambda: Lambda {
+                    arn: "arn".to_string(),
+                    request_id: "request_id".to_string(),
+                },
+                timestamp: 0,
+                status: "status".to_string(),
+            },
+            hostname: "hostname".to_string(),
+            service: "service".to_string(),
+            tags: "tags".to_string(),
+            source: "source".to_string(),
+        };
+        aggregator.add(log.clone());
+        assert_eq!(aggregator.messages.len(), 1);
+        assert_eq!(aggregator.messages[0], log);
+    }
+
+    #[test]
+    fn get_batch() {
+        let mut aggregator = Aggregator::new();
+        let log = IntakeLog {
+            message: LambdaMessage {
+                message: "test".to_string(),
+                lambda: Lambda {
+                    arn: "arn".to_string(),
+                    request_id: "request_id".to_string(),
+                },
+                timestamp: 0,
+                status: "status".to_string(),
+            },
+            hostname: "hostname".to_string(),
+            service: "service".to_string(),
+            tags: "tags".to_string(),
+            source: "source".to_string(),
+        };
+        aggregator.add(log.clone());
+        assert_eq!(aggregator.messages.len(), 1);
+        let batch = aggregator.get_batch();
+        assert_eq!(batch.len(), 1);
+        assert_eq!(batch[0], log);
+        assert_eq!(aggregator.messages.len(), 0);
+    }
+}
