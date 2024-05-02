436e53333656e406ad4db996c8647f8eea56b4c0
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2024-05-02 18:15:31 -0400

    process more telemetry events
    runtime done, start, init start, extension, function, and report

--- bottlecap/src/logs/processor.rs
+++ bottlecap/src/logs/processor.rs
@@ -42,28 +42,78 @@ impl Processor {
     }
 
     pub fn process(&self, event: TelemetryEvent) -> Result<IntakeLog, Box<dyn Error>> {
+        let mut log = IntakeLog {
+            hostname: self.function_arn.clone(),
+            source: LOGS_SOURCE.to_string(),
+            service: "placeholder-service".to_string(), // TODO: replace with config service
+            tags: "placeholder:tags".to_string(),       // TODO: replace with config tags
+            message: LambdaMessage {
+                message: "placeholder-message".to_string(),
+                lambda: Lambda {
+                    arn: self.function_arn.clone(),
+                    request_id: "placeholder-request-id".to_string(), // TODO: replace with incoming request id
+                },
+                timestamp: event.time.timestamp_millis(),
+                status: "placeholder-status".to_string(),
+            },
+        };
         match event.record {
+            TelemetryRecord::Function(v) | TelemetryRecord::Extension(v) => {
+                log.message.status = "info".to_string();
+                log.message.message = v;
+
+                Ok(log)
+            }
+            TelemetryRecord::PlatformInitStart {
+                runtime_version,
+                runtime_version_arn,
+                .. // TODO: check if we could do something with this metrics: `initialization_type` and `phase`
+            } => {
+                let rv = runtime_version.unwrap_or("?".to_string()); // TODO: check what does containers display
+                let rv_arn = runtime_version_arn.unwrap_or("?".to_string()); // TODO: check what do containers display
+                log.message.message = format!("INIT_START Runtime Version: {} Runtime Version ARN: {}", rv, rv_arn);
+
+                Ok(log)
+            },
             TelemetryRecord::PlatformStart {
                 request_id,
                 version,
             } => {
                 let version = version.unwrap_or("$LATEST".to_string());
-                Ok(IntakeLog {
-                    message: LambdaMessage {
-                        message: format!("START RequestId: {} Version: {}", request_id, version),
-                        lambda: Lambda {
-                            arn: self.function_arn.clone(),
-                            request_id,
-                        },
-                        timestamp: event.time.timestamp_millis(),
-                        status: "info".to_string(),
-                    },
-                    hostname: self.function_arn.clone(),
-                    service: "TODO-add-service".to_string(),
-                    tags: "todo:add-tags".to_string(),
-                    source: LOGS_SOURCE.to_string(),
-                })
-            }
+                log.message.message =
+                    format!("START RequestId: {} Version: {}", request_id, version);
+                log.message.lambda.request_id = request_id;
+                log.message.status = "info".to_string();
+
+                Ok(log)
+            },
+            TelemetryRecord::PlatformRuntimeDone { request_id , .. } => {  // TODO: check what to do with rest of the fields
+                log.message.message = format!("END RequestId: {}", request_id);
+                log.message.lambda.request_id = request_id;
+                log.message.status = "info".to_string();
+                Ok(log)
+            },
+            TelemetryRecord::PlatformReport { request_id, metrics, .. } => { // TODO: check what to do with rest of the fields
+                let mut message = format!(
+                    "REPORT RequestId: {} Duration: {} ms Billed Duration: {} ms Memory Size: {} MB Max Memory Used: {} MB",
+                    request_id,
+                    metrics.duration_ms,
+                    metrics.billed_duration_ms,
+                    metrics.memory_size_mb,
+                    metrics.max_memory_used_mb,
+                );
+
+                let init_duration_ms = metrics.init_duration_ms;
+                if let Some(init_duration_ms) = init_duration_ms {
+                    message = format!("{} Init Duration: {} ms", message, init_duration_ms)
+                }
+
+                log.message.message = message;
+                log.message.lambda.request_id = request_id;
+                log.message.status = "info".to_string();
+
+                Ok(log)
+            },
             _ => Err("Unsupported event type".into()),
         }
     }
