90acff1bdd8c10dd338e9b24ae89480181b32909
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2024-05-05 21:18:20 -0400

    modify `datadog.rs` to use config site

--- bottlecap/src/logs/datadog.rs
+++ bottlecap/src/logs/datadog.rs
@@ -1,39 +1,39 @@
-use std::{env, error::Error};
+use std::error::Error;
 
 use tracing::{debug, error};
 
 use crate::logs::processor::IntakeLog;
 
-const LOGS_API_INTAKE: &str = "https://http-intake.logs.datadoghq.com/api/v2/logs";
-
 pub struct DdApi {
     api_key: String,
+    site: String,
     ureq_agent: ureq::Agent,
 }
 
 impl DdApi {
-    pub fn new() -> Self {
+    pub fn new(api_key: String, site: String) -> Self {
         DdApi {
-            api_key: env::var("DD_API_KEY").expect("unable to read envvars, catastrophic failure"),
+            api_key,
+            site,
             ureq_agent: ureq::AgentBuilder::new().build(),
         }
     }
 
     pub fn send(&self, logs: &Vec<IntakeLog>) -> Result<(), Box<dyn Error>> {
-        let api_key = self.api_key.clone();
+        let url = format!("https://http-intake.logs.{}/api/v2/logs", &self.site);
 
         if !logs.is_empty() {
             let resp: Result<ureq::Response, ureq::Error> = self
                 .ureq_agent
-                .post(LOGS_API_INTAKE)
-                .set("DD-API-KEY", &api_key)
+                .post(&url)
+                .set("DD-API-KEY", &self.api_key)
                 .set("DD-PROTOCOL", "agent-json")
                 .set("Content-Type", "application/json")
                 .send_json(logs);
 
             match resp {
                 Ok(resp) => {
-                    if resp.status() != 200 {
+                    if resp.status() != 202 {
                         debug!("Failed to send logs to datadog: {}", resp.status());
                     }
                 }
