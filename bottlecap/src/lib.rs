bbfc6240870a26f83b73e186093b9cc49f421db2
Brian L. Troutwine <brian.troutwine@datadoghq.com>
2024-05-09 12:09:06 -0700

    Move `main.rs` into a binary path
    This commit moves the `main.rs` down into `src/bin/bottlecap` allowing for code
    solely used by `bottlecap` binary to be migrated into that directory, for
    additional binaries to be created in the project on the same pattern and for the
    `bottlecap` crate to be built indepdently of its binary, useful for micro
    benchmarks and the like.
    
    If it becomes desirable we can make a workspace with multiple sub-crates, in the
    manner of https://github.com/DataDog/single-machine-performance or
    https://github.com/DataDog/saluki/. I don't know that this is necessary right
    now.
    
    Signed-off-by: Brian L. Troutwine <brian.troutwine@datadoghq.com>

---
+++ bottlecap/src/lib.rs
@@ -0,0 +1,34 @@
+///! Crate for the `bottlecap` project
+pub mod config;
+pub mod event_bus;
+pub mod events;
+pub mod lifecycle;
+pub mod logger;
+pub mod logs;
+pub mod metrics;
+pub mod telemetry;
+
+use std::{env, io};
+
+pub const EXTENSION_HOST: &str = "0.0.0.0";
+pub const EXTENSION_NAME: &str = "datadog-agent";
+pub const EXTENSION_FEATURES: &str = "accountId";
+pub const EXTENSION_NAME_HEADER: &str = "Lambda-Extension-Name";
+pub const EXTENSION_ID_HEADER: &str = "Lambda-Extension-Identifier";
+pub const EXTENSION_ACCEPT_FEATURE_HEADER: &str = "Lambda-Extension-Accept-Feature";
+pub const EXTENSION_ROUTE: &str = "2020-01-01/extension";
+
+// todo: make sure we can override those with environment variables
+pub const DOGSTATSD_PORT: u16 = 8185;
+
+pub const TELEMETRY_SUBSCRIPTION_ROUTE: &str = "2022-07-01/telemetry";
+pub const TELEMETRY_PORT: u16 = 8124;
+
+pub fn base_url(route: &str) -> io::Result<String> {
+    Ok(format!(
+        "http://{}/{}",
+        env::var("AWS_LAMBDA_RUNTIME_API")
+            .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e.to_string()))?,
+        route
+    ))
+}
