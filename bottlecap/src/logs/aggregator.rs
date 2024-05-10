3117fd8976d467a60e096ae6c1628b2323702a40
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2024-05-10 11:00:45 -0400

    solve conflicts generated by merged

--- bottlecap/src/logs/aggregator.rs
+++ bottlecap/src/logs/aggregator.rs
@@ -145,7 +145,7 @@ mod tests {
                 message: "test".to_string(),
                 lambda: Lambda {
                     arn: "arn".to_string(),
-                    request_id: "request_id".to_string(),
+                    request_id: Some("request_id".to_string()),
                 },
                 timestamp: 0,
                 status: "status".to_string(),
@@ -182,7 +182,7 @@ mod tests {
                 message: "test".to_string(),
                 lambda: Lambda {
                     arn: "arn".to_string(),
-                    request_id: "request_id".to_string(),
+                    request_id: Some("request_id".to_string()),
                 },
                 timestamp: 0,
                 status: "status".to_string(),
