797c910168a5bcac0fec3d89218bbc52df77be41
alexgallotta <5581237+alexgallotta@users.noreply.github.com>
2024-05-15 14:10:29 -0400

    reorg runner for bottlecap

--- bottlecap-run/runBottlecap.sh
+++ bottlecap-run/runBottlecap.sh
@@ -2,20 +2,23 @@
 
 set -e
 arch=$(uname -a)
-cd bottlecap 
+cd ../bottlecap
 # build bottlecap in debug mode
 if (echo $arch | grep -q "Darwin"); then
     PATH=/usr/bin:$PATH cargo zigbuild --target=aarch64-unknown-linux-gnu
-    build_path=bottlecap/target/aarch64-unknown-linux-gnu/debug/bottlecap
+    build_path=../bottlecap/target/aarch64-unknown-linux-gnu/debug/bottlecap
 else
     cargo build
-    build_path=bottlecap/target/debug/bottlecap
+    build_path=../bottlecap/target/debug/bottlecap
 fi
-cd ..
+cd -
 
 # run a hello world function in Lambda RIE (https://github.com/aws/aws-lambda-runtime-interface-emulator)
 # the lambda_extension binary is copied to /opt/extensions
-docker_name=$(docker create --publish 9000:8080 "public.ecr.aws/lambda/nodejs:20" "index.handler")
+docker_name=$(docker create \
+  --publish 9000:8080 \
+  -e DD_API_KEY=XXX \
+  "public.ecr.aws/lambda/nodejs:20" "index.handler")
 echo -e 'export const handler = async () => {\n\tconsole.log("Hello world!");\n};' > /tmp/index.mjs
 docker cp "/tmp/index.mjs" "${docker_name}:/var/task/index.mjs"
 docker start "${docker_name}"           
