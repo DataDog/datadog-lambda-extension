65ce0f60fda9b6f09d0e4935f11ee82c272df55e
alexgallotta <5581237+alexgallotta@users.noreply.github.com>
2024-05-16 14:13:47 -0400

    make sure permissions are right

--- scripts/bottlecap/build.sh
+++ scripts/bottlecap/build.sh
@@ -2,19 +2,28 @@
 set -e
 
 function zip_extension {
-  local LAYER_DIR=../../.layers/datadog_bottlecap-amd64
+  local ARCH=amd64
+  local LAYER_DIR=../../.layers/datadog_bottlecap-$ARCH
   mkdir -p $LAYER_DIR
 
-  docker_id=$(docker create public.ecr.aws/datadog/lambda-extension:57 --entrypoint /bin/bash)
+  docker_id=$(docker create public.ecr.aws/datadog/lambda-extension:57 --platform linux/$ARCH --entrypoint /bin/bash)
   docker cp $docker_id:/opt/extensions/datadog-agent $LAYER_DIR/datadog-agent-go
   docker rm -f $docker_id
 
   cp ../datadog_wrapper $LAYER_DIR/datadog_wrapper
 
   mkdir -p $LAYER_DIR/extensions
-  cp ../../bottlecap/bottlecap-main $LAYER_DIR/extensions/datadog-agent
-  cd $LAYER_DIR/..
-  zip -r datadog_bottlecap-amd64.zip datadog_bottlecap-amd64
+  cd ../../bottlecap/target/release/
+  cargo build --release
+  cd -
+  cp ../../bottlecap/target/release/bottlecap $LAYER_DIR/extensions/datadog-agent
+  cd $LAYER_DIR
+
+  chmod 755 datadog_wrapper datadog-agent-go extensions/datadog-agent
+
+  zip -r datadog_bottlecap-$ARCH.zip datadog_wrapper datadog-agent-go extensions/datadog-agent
+  cd ..
+  mv datadog_bottlecap-$ARCH/datadog_bottlecap-$ARCH.zip .
 }
 
 zip_extension
