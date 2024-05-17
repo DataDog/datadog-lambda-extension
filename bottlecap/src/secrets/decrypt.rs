5930709433eec254a9e579a8b44c37dc6ca53578
alexgallotta <5581237+alexgallotta@users.noreply.github.com>
2024-05-17 12:30:40 -0400

    add manual req for secret (still broken)

--- bottlecap/src/secrets/decrypt.rs
+++ bottlecap/src/secrets/decrypt.rs
@@ -4,8 +4,13 @@ use aws_config;
 use aws_sdk_secretsmanager::Client;
 use aws_sdk_secretsmanager;
 use tracing::debug;
-use std::thread;
+use std::{env, thread};
+use std::time::Instant;
 
+use chrono::{Utc};
+use hmac::{Hmac, Mac};
+use reqwest::header::{HeaderMap, HeaderValue};
+use sha2::{Digest, Sha256};
 
 type ResolveFn = fn(String) -> Result<String, Error>;
 
@@ -17,8 +22,11 @@ pub fn resolve_secrets(config: Config, decoder: ResolveFn) -> Result<Config, Str
         if !config.api_key_secret_arn.is_empty() {
             debug!("DD_API_KEY_SECRET_ARN found, trying to resolve ARN secret");
             let string = config.api_key_secret_arn.clone();
+
+            let before_awssm = Instant::now();
+
             let wait_fot_ret = thread::spawn(move || { decoder(string) });
-            match wait_fot_ret.join() {
+            let res = match wait_fot_ret.join() {
                 Ok(Ok(secret)) => {
                     Ok(Config {
                         api_key: secret,
@@ -36,11 +44,118 @@ pub fn resolve_secrets(config: Config, decoder: ResolveFn) -> Result<Config, Str
                         Err("Thread panicked".to_string())
                     }
                 }
-            }
+            };
+
+            let duration_awssm = before_awssm.elapsed();
+            println!("AWS SM took {}ms", duration_awssm.as_millis());
+
+            let before_manual = Instant::now();
+            let manual_res = manual_decrypt(config.api_key_secret_arn);
+            let duration_manual = before_manual.elapsed();
+            println!("Manual took {}ms", duration_manual.as_millis());
+
+            println!("Secrets are equal? {}", res.clone().unwrap().api_key == manual_res);
+
+            return res;
         } else {
             Err("No API key or secret ARN found".to_string())
         }
     }
+}
+
+
+type HmacSha256 = Hmac<Sha256>;
+
+fn sign(key: &[u8], msg: &str) -> Vec<u8> {
+    let mut mac = HmacSha256::new_from_slice(key).expect("HMAC can take key of any size");
+    mac.update(msg.as_bytes());
+    mac.finalize().into_bytes().to_vec()
+}
+
+fn get_signature_key(key: &str, date_stamp: &str, region_name: &str, service_name: &str) -> Vec<u8> {
+    let k_date = sign(format!("AWS4{}", key).as_bytes(), date_stamp);
+    let k_region = sign(&k_date, region_name);
+    let k_service = sign(&k_region, service_name);
+    sign(&k_service, "aws4_request")
+}
+
+
+fn manual_decrypt(secret_arn: String) -> String {
+    let region = "us-east-1";
+
+    let access_key = env::var("AWS_ACCESS_KEY_ID").expect("AWS_ACCESS_KEY_ID not set");
+    let secret_key = env::var("AWS_SECRET_ACCESS_KEY").expect("AWS_SECRET_ACCESS_KEY not set");
+
+    let service = "secretsmanager";
+    let host = format!("secretsmanager.{}.amazonaws.com", region);
+    let endpoint = format!("https://{}", host);
+
+    let t = Utc::now();
+    let amz_date = t.format("%Y%m%dT%H%M%SZ").to_string();
+    let date_stamp = t.format("%Y%m%d").to_string();
+
+    let canonical_uri = "/";
+    let canonical_querystring = format!("SecretId={}", secret_arn.split(":secret:").nth(1).unwrap());
+    let canonical_headers = format!("host:{}\nx-amz-date:{}\nx-amz-target:secretsmanager.GetSecretValue\n",
+                                    host, amz_date);
+    let signed_headers = "host;x-amz-date;x-amz-target";
+    let payload_hash = Sha256::digest("".as_bytes());
+    let payload_hash_hex = hex::encode(payload_hash);
+
+    let canonical_request = format!(
+        "GET\n{}\n{}\n{}\n{}\n{}",
+        canonical_uri, canonical_querystring, canonical_headers, signed_headers, payload_hash_hex
+    );
+
+    let algorithm = "AWS4-HMAC-SHA256";
+    let credential_scope = format!("{}/{}/{}/aws4_request", date_stamp, region, service);
+    let string_to_sign = format!(
+        "{}\n{}\n{}\n{}",
+        algorithm,
+        amz_date,
+        credential_scope,
+        hex::encode(Sha256::digest(canonical_request.as_bytes()))
+    );
+
+    let signing_key = get_signature_key(&secret_key, &date_stamp, region, service);
+
+    let mut mac = HmacSha256::new_from_slice(&signing_key).expect("HMAC can take key of any size");
+    mac.update(string_to_sign.as_bytes());
+    let signature = hex::encode(mac.finalize().into_bytes());
+
+    let authorization_header = format!(
+        "{} Credential={}/{}, SignedHeaders={}, Signature={}",
+        algorithm, access_key, credential_scope, signed_headers, signature
+    );
+
+    let client = reqwest::Client::new();
+    let mut headers = HeaderMap::new();
+    headers.insert("x-amz-date", HeaderValue::from_str(&amz_date).unwrap());
+    headers.insert("Authorization", HeaderValue::from_str(&authorization_header).unwrap());
+    headers.insert("x-amz-target", HeaderValue::from_str("secretsmanager.GetSecretValue").unwrap());
+    headers.insert("Content-Type", HeaderValue::from_str("application/x-amz-json-1.1").unwrap());
+
+    let request_url = format!("{}/?{}", endpoint, canonical_querystring);
+    let this_thread_runtime = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    let json_body = &serde_json::json!({ "SecretId": secret_arn.split(":secret:").nth(1).unwrap()});
+    println!("json body {}", json_body);
+
+    let req = client
+        .post(&request_url)
+        .body(json_body.to_string())
+        .headers(headers);
+
+    println!("Request: {:?}", req);
+
+    let body = this_thread_runtime.block_on(req.send()).unwrap();
+
+    println!("Response: {:?}", body);
+
+    return "test".to_string();
 }
 
 pub fn decrypt_secret_arn(secret_arn: String) -> Result<String, Error> {
@@ -65,7 +180,6 @@ pub fn decrypt_secret_arn(secret_arn: String) -> Result<String, Error> {
                                  format!("No secret found: {}", e)))
     }
 }
-
 
 #[cfg(test)]
 mod tests {
