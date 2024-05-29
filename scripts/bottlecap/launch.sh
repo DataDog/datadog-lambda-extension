5a6ea987b77a5ff1b66c120e6bac57a281b47293
alexgallotta <5581237+alexgallotta@users.noreply.github.com>
2024-05-29 13:21:11 -0400

    add scripts to force restarts

--- scripts/bottlecap/launch.sh
+++ scripts/bottlecap/launch.sh
@@ -1,34 +1,49 @@
 #!/bin/bash
 set -e
 
-FUNCTION_NAME="agbtc2-bottlecap-busy-node20-lambda"
+FUNCTION_NAME="svls4825m-bottlecap-busy-node20-lambda"
 
 source deploy_steps.sh
 
 update() {
   aws-vault exec sso-serverless-sandbox-account-admin -- aws lambda update-function-configuration \
     --function-name $FUNCTION_NAME \
-    --layers "arn:aws:lambda:${REGION}:425362996713:layer:${LAYER_NAME}:${1}"
+    --layers "arn:aws:lambda:${REGION}:425362996713:layer:${LAYER_NAME}:${1}" "arn:aws:lambda:us-east-1:425362996713:layer:Datadog-Node20-x:116"
   echo "DONE: Updated function ${FUNCTION_NAME} with layer ${LAYER_NAME} version ${1}"
 }
 
+force_update() {
+  local F_NAME=${1}
+  local TIMEOUT=$((${2} + 30))
+  aws-vault exec sso-serverless-sandbox-account-admin -- aws lambda update-function-configuration \
+    --function-name ${F_NAME} \
+    --timeout $TIMEOUT > /dev/null
+}
+
 invoke(){
-  aws-vault exec sso-serverless-sandbox-account-admin -- aws lambda invoke \
-    --function-name ${FUNCTION_NAME} \
+  local F_NAME=${1}
+  aws-vault exec sso-serverless-sandbox-account-admin -- time aws lambda invoke \
+    --function-name ${F_NAME} \
     --payload '{"message": "Hello from Lambda!"}' \
     /dev/stdout
-  echo "DONE: Invoked function ${FUNCTION_NAME}"
+  echo "DONE: Invoked function ${F_NAME}"
 }
 
 all(){
   time zip_extension
   version_nbr=$(publish)
   update "$version_nbr"
-  invoke
+  invoke FUNCTION_NAME
+}
+
+cold_call() {
+    for i in {1..10}; do
+      force_update $FUNCTION_NAME $i && invoke $FUNCTION_NAME
+    done
 }
 
 call() {
-  invoke
+  invoke $FUNCTION_NAME
 }
 
 "$@"
