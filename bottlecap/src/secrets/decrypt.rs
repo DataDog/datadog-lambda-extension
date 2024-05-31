0a016376a2ba1ddff895934f3d8737d4f9fd0419
alexgallotta <5581237+alexgallotta@users.noreply.github.com>
2024-05-31 09:55:21 -0400

    add kms handling

--- bottlecap/src/secrets/decrypt.rs
+++ bottlecap/src/secrets/decrypt.rs
@@ -2,52 +2,60 @@ use crate::config::Config;
 use base64::prelude::*;
 use chrono::{DateTime, Utc};
 use hmac::{Hmac, Mac};
+use reqwest::blocking::Client;
 use reqwest::header::{HeaderMap, HeaderValue};
-use reqwest::Client;
 use serde_json::Value;
 use sha2::{Digest, Sha256};
 use std::env;
-use std::io::Error;
+use std::io::{Error, Result};
 use std::time::Instant;
 use tracing::debug;
 
-pub async fn resolve_secrets(config: Config) -> Result<Config, Box<dyn std::error::Error>> {
+pub fn resolve_secrets(config: Config) -> Result<Config> {
     if !config.api_key.is_empty() {
         debug!("DD_API_KEY found, not trying to resolve secrets");
         Ok(config)
     } else if !config.api_key_secret_arn.is_empty() || !config.kms_api_key.is_empty() {
         let before_decrypt = Instant::now();
 
-        let client = &Client::builder().use_rustls_tls().build()?;
+        let client = Client::builder()
+            .use_rustls_tls()
+            .build()
+            .expect("Failed to create reqwest client for aws decrypt");
 
         let aws_config = AwsConfig {
-            region: env::var("AWS_DEFAULT_REGION")?,
-            aws_access_key_id: env::var("AWS_ACCESS_KEY_ID")?,
-            aws_secret_access_key: env::var("AWS_SECRET_ACCESS_KEY")?,
-            aws_session_token: env::var("AWS_SESSION_TOKEN")?,
-            function_name: env::var("AWS_LAMBDA_FUNCTION_NAME")?,
+            region: env::var("AWS_DEFAULT_REGION").expect("AWS_DEFAULT_REGION not set"),
+            aws_access_key_id: env::var("AWS_ACCESS_KEY_ID").expect("AWS_ACCESS_KEY_ID not set"),
+            aws_secret_access_key: env::var("AWS_SECRET_ACCESS_KEY")
+                .expect("AWS_SECRET_ACCESS_KEY not set"),
+            aws_session_token: env::var("AWS_SESSION_TOKEN")
+                .expect("AWS_SESSION_TOKEN is not set!"),
+            function_name: env::var("AWS_LAMBDA_FUNCTION_NAME")
+                .expect("AWS_LAMBDA_FUNCTION_NAME is not set!"),
         };
 
-        let decrypted_key: String = if config.api_key_secret_arn.is_empty() {
-            decrypt_aws_kms(client, config.kms_api_key.clone(), aws_config).await?
+        let decrypt_method = if config.api_key_secret_arn.is_empty() {
+            decrypt_aws_kms
         } else {
-            decrypt_aws_sm(client, config.api_key_secret_arn.clone(), aws_config).await?
+            decrypt_aws_sm
         };
 
+        let resolved_key = decrypt_method(client, config.api_key_secret_arn.clone(), aws_config)
+            .expect("Failed to decrypt secret");
         debug!("Decrypt took {}ms", before_decrypt.elapsed().as_millis());
 
         Ok(Config {
-            api_key: decrypted_key,
+            api_key: resolved_key,
             ..config.clone()
         })
     } else {
         Err(Error::new(
             std::io::ErrorKind::InvalidInput,
             "No API key or secret ARN found".to_string(),
-        )
-        .into())
+        ))
     }
 }
+
 
 struct RequestArgs<'a> {
     service: String,
@@ -64,41 +72,35 @@ struct AwsConfig {
     function_name: String,
 }
 
-async fn decrypt_aws_kms(
-    client: &Client,
-    kms_key: String,
-    aws_config: AwsConfig,
-) -> Result<String, Box<dyn std::error::Error>> {
-    let json_body = &serde_json::json!({
-        "CiphertextBlob": kms_key,
-        "encryptionContext": { "LambdaFunctionName": aws_config.function_name }}
-    );
+fn decrypt_aws_kms(client: Client, kms_key: String, aws_config: AwsConfig) -> Result<String> {
+    let decoded_key = BASE64_STANDARD
+        .decode(kms_key)
+        .expect("Failed to decode base64 string");
+    let decoded_key_str = String::from_utf8(decoded_key).expect("Failed to convert to String");
+
+    let json_body = &serde_json::json!({ "CiphertextBlob": decoded_key_str });
 
     let headers = build_get_secret_signed_headers(
         &aws_config,
         RequestArgs {
-            service: "kms".to_string(),
+            service: format!("kms.{}.amazonaws.com", aws_config.region),
             body: json_body,
             time: Utc::now(),
             x_amz_target: "TrentService.Decrypt".to_string(),
         },
     );
 
-    let v = request(json_body, headers?, client).await?;
+    let v = request(json_body, headers, client);
 
-    if let Some(secret_string_b64) = v["Plaintext"].as_str() {
-        let secret_string = String::from_utf8(BASE64_STANDARD.decode(secret_string_b64)?)?;
-        Ok(secret_string)
+    return if let Some(secret_string) = v["CiphertextBlob"].as_str() {
+        debug!("{}", secret_string.to_string());
+        Ok(secret_string.to_string())
     } else {
-        Err(Error::new(std::io::ErrorKind::InvalidData, v.to_string()).into())
-    }
+        Err(Error::new(std::io::ErrorKind::InvalidData, v.to_string()))
+    };
 }
 
-async fn decrypt_aws_sm(
-    client: &Client,
-    secret_arn: String,
-    aws_config: AwsConfig,
-) -> Result<String, Box<dyn std::error::Error>> {
+fn decrypt_aws_sm(client: Client, secret_arn: String, aws_config: AwsConfig) -> Result<String> {
     let json_body = &serde_json::json!({ "SecretId": secret_arn});
 
     let headers = build_get_secret_signed_headers(
@@ -111,45 +113,37 @@ async fn decrypt_aws_sm(
         },
     );
 
-    let v = request(json_body, headers?, client)
-        .await
-        .map_err(|err| Error::new(std::io::ErrorKind::InvalidData, err.to_string()))?;
+    let v = request(json_body, headers, client);
 
-    if let Some(secret_string) = v["SecretString"].as_str() {
+    return if let Some(secret_string) = v["SecretString"].as_str() {
         Ok(secret_string.to_string())
     } else {
-        Err(Error::new(std::io::ErrorKind::InvalidData, v.to_string()).into())
-    }
+        Err(Error::new(std::io::ErrorKind::InvalidData, v.to_string()))
+    };
 }
 
-async fn request(
-    json_body: &Value,
-    headers: HeaderMap,
-    client: &Client,
-) -> Result<Value, Box<dyn std::error::Error>> {
-    let host_header = &headers["host"]
-        .to_str()
-        .map_err(|err| Error::new(std::io::ErrorKind::InvalidInput, err.to_string()))?;
+fn request(json_body: &Value, headers: HeaderMap, client: Client) -> Value {
     let req = client
-        .post(format!("https://{host_header}"))
+        .post(format!(
+            "https://{}",
+            &headers["host"].to_str().expect("invalid host")
+        ))
         .json(json_body)
         .headers(headers);
 
-    let body = req
-        .send()
-        .await
-        .map_err(|err| Error::new(std::io::ErrorKind::InvalidData, err.to_string()))?
+    let resp = req.send();
+    let body = resp
+        .expect("Failed to get response body")
         .text()
-        .await
-        .map_err(|err| Error::new(std::io::ErrorKind::InvalidData, err.to_string()))?;
-    let v: Value = serde_json::from_str(&body)?;
-    Ok(v)
+        .expect("Cannot deserialize body");
+    let v: Value = serde_json::from_str(&body).expect("Failed to parse JSON");
+    v
 }
 
 fn build_get_secret_signed_headers(
     aws_config: &AwsConfig,
     header_values: RequestArgs,
-) -> Result<HeaderMap, Box<dyn std::error::Error>> {
+) -> HeaderMap {
     let amz_date = header_values.time.format("%Y%m%dT%H%M%SZ").to_string();
     let date_stamp = header_values.time.format("%Y%m%d").to_string();
     let host = format!(
@@ -188,9 +182,9 @@ fn build_get_secret_signed_headers(
         &date_stamp,
         aws_config.region.as_str(),
         header_values.service.as_str(),
-    )?;
+    );
 
-    let signature = hex::encode(sign(&signing_key, &string_to_sign)?);
+    let signature = hex::encode(sign(&signing_key, &string_to_sign));
 
     let authorization_header = format!(
         "{} Credential={}/{}, SignedHeaders={}, Signature={}",
@@ -199,34 +193,32 @@ fn build_get_secret_signed_headers(
     let mut headers = HeaderMap::new();
     headers.insert(
         "Authorization",
-        HeaderValue::from_str(&authorization_header)?,
+        HeaderValue::from_str(&authorization_header).expect("invalid authorization"),
     );
-    headers.insert("host", HeaderValue::from_str(&host)?);
+    headers.insert("host", HeaderValue::from_str(&host).expect("invalid host"));
     headers.insert(
         "Content-Type",
-        HeaderValue::from_str("application/x-amz-json-1.1")?,
+        HeaderValue::from_str("application/x-amz-json-1.1").expect("invalid content-type"),
     );
-    headers.insert("x-amz-date", HeaderValue::from_str(&amz_date)?);
+    headers.insert(
+        "x-amz-date",
+        HeaderValue::from_str(&amz_date).expect("invalid x-amz-date"),
+    );
     headers.insert(
         "x-amz-target",
-        HeaderValue::from_str(header_values.x_amz_target.as_str())?,
+        HeaderValue::from_str(header_values.x_amz_target.as_str()).expect("invalid x-amz-target"),
     );
     headers.insert(
         "x-amz-security-token",
-        HeaderValue::from_str(&aws_config.aws_session_token)?,
+        HeaderValue::from_str(&aws_config.aws_session_token).expect("invalid x-amz-security-token"),
     );
-    Ok(headers)
+    headers
 }
 
-fn sign(key: &[u8], msg: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
-    let mut mac = Hmac::<Sha256>::new_from_slice(key).map_err(|err| {
-        Error::new(
-            std::io::ErrorKind::InvalidInput,
-            format!("Error creating HMAC: {err}"),
-        )
-    })?;
+fn sign(key: &[u8], msg: &str) -> Vec<u8> {
+    let mut mac = Hmac::<Sha256>::new_from_slice(key).expect("HMAC can take key of any size");
     mac.update(msg.as_bytes());
-    Ok(mac.finalize().into_bytes().to_vec())
+    mac.finalize().into_bytes().to_vec()
 }
 
 fn get_aws4_signature_key(
@@ -234,10 +226,10 @@ fn get_aws4_signature_key(
     date_stamp: &str,
     region_name: &str,
     service_name: &str,
-) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
-    let k_date = sign(format!("AWS4{key}").as_bytes(), date_stamp)?;
-    let k_region = sign(&k_date, region_name)?;
-    let k_service = sign(&k_region, service_name)?;
+) -> Vec<u8> {
+    let k_date = sign(format!("AWS4{key}").as_bytes(), date_stamp);
+    let k_region = sign(&k_date, region_name);
+    let k_service = sign(&k_region, service_name);
     sign(&k_service, "aws4_request")
 }
 
@@ -260,12 +252,12 @@ mod tests {
                 function_name: "arn:some-function".to_string(),
             },
             RequestArgs {
-                service: "secretsmanager".to_string(),
+                service: "secretsmanager.us-east-1.amazonaws.com".to_string(),
                 body: &serde_json::json!({ "SecretId": "arn:aws:secretsmanager:region:account-id:secret:secret-name"}),
                 time,
                 x_amz_target: "secretsmanager.GetSecretValue".to_string(),
             },
-        ).unwrap();
+        );
 
         let mut expected_headers = HeaderMap::new();
         expected_headers.insert("authorization", HeaderValue::from_str("AWS4-HMAC-SHA256 Credential=AKIDEXAMPLE/20240530/us-east-1/secretsmanager/aws4_request, SignedHeaders=content-type;host;x-amz-date;x-amz-security-token;x-amz-target, Signature=63d50106f9c0ab1f02c1f81d6c720b01bce369d45f63a8f6280ffe7945405b81").unwrap());
