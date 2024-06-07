ae570a0431ef67d65d90410f39ca3d45782e8541
alexgallotta <5581237+alexgallotta@users.noreply.github.com>
2024-06-07 14:30:42 -0400

    Refactor building (#267)
    * refactor and remove useless code
    
    * add dockerized build
    
    * example usage
    
    * remove output
    
    * clean up target to avoid invaliding cache and copying gbs

--- scripts/build_bottlecap_layer.sh
+++ scripts/build_bottlecap_layer.sh
@@ -12,23 +12,6 @@
 
 set -e
 
-validate_argument() {
-  if [ -z "$VERSION" ]; then
-      echo "Extension version not specified"
-      echo ""
-      echo "EXITING SCRIPT."
-      exit 1
-  fi
-
-  if [ -z "$BUILD_TAGS" ]; then
-      BUILD_TAGS="serverless otlp"
-  fi
-
-  if [ -z "$AGENT_PATH" ]; then
-      AGENT_PATH="../datadog-agent"
-  fi
-}
-
 prepare_folders() {
   # Move into the root directory, so this script can be called from any directory
   SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
@@ -46,7 +29,7 @@ prepare_folders() {
 }
 
 
-docker_build_bottlecap_zip() {
+_docker_build_bottlecap_zip() {
     local arch=$1
     if [ "$arch" == "amd64" ]; then
         PLATFORM="x86_64"
@@ -55,11 +38,11 @@ docker_build_bottlecap_zip() {
     fi
 
     docker build --platform linux/${arch} \
-        -t datadog/build-bottlecap-${arch}:$VERSION \
+        -t datadog/build-bottlecap-${arch} \
         -f ./scripts/Dockerfile.bottlecap.build \
         --build-arg PLATFORM=$PLATFORM \
         . --load
-    local dockerId=$(docker create datadog/build-bottlecap-${arch}:$VERSION)
+    local dockerId=$(docker create datadog/build-bottlecap-${arch})
     docker cp $dockerId:/datadog_extension.zip $TARGET_DIR/datadog_bottlecap-${arch}.zip
     docker rm $dockerId
     unzip $TARGET_DIR/datadog_bottlecap-${arch}.zip -d $TARGET_DIR/datadog_bottlecap-${arch}
@@ -68,17 +51,16 @@ docker_build_bottlecap_zip() {
 build_for_arch() {
   if [ "$ARCHITECTURE" == "amd64" ]; then
       echo "Building for amd64 only"
-      docker_build_bottlecap_zip amd64
+      _docker_build_bottlecap_zip amd64
   elif [ "$ARCHITECTURE" == "arm64" ]; then
       echo "Building for arm64 only"
-      docker_build_bottlecap_zip arm64
+      _docker_build_bottlecap_zip arm64
   else
       echo "Building for both amd64 and arm64"
-      docker_build_bottlecap_zip amd64
-      docker_build_bottlecap_zip arm64
+      _docker_build_bottlecap_zip amd64
+      _docker_build_bottlecap_zip arm64
   fi
 }
 
-validate_argument
 prepare_folders
 build_for_arch
--- scripts/publish_bottlecap_sandbox.sh
+++ scripts/publish_bottlecap_sandbox.sh
@@ -14,55 +14,43 @@
 
 set -e
 
-if [ "$ARCHITECTURE" == "amd64" ]; then
-    echo "Publishing for amd64 only"
-    LAYER_PATH=".layers/datadog_bottlecap-amd64.zip"
-    LAYER_NAME="Datadog-Bottlecap-Beta"
-elif [ "$ARCHITECTURE" == "arm64" ]; then
-    echo "Publishing for arm64 only"
-    LAYER_PATH=".layers/datadog_bottlecap-arm64.zip"
-    LAYER_NAME="Datadog-Bottlecap-Beta-ARM"
-fi
-if [ -z $ARCHITECTURE ]; then
-    echo "No architecture specified, defaulting to amd64"
-    ARCHITECTURE="amd64"
-fi
+_init_arg(){
+  if [ "$ARCHITECTURE" == "amd64" ]; then
+      echo "Publishing for amd64 only"
+      LAYER_PATH=".layers/datadog_bottlecap-amd64.zip"
+      LAYER_NAME="Datadog-Bottlecap-Beta"
+  elif [ "$ARCHITECTURE" == "arm64" ]; then
+      echo "Publishing for arm64 only"
+      LAYER_PATH=".layers/datadog_bottlecap-arm64.zip"
+      LAYER_NAME="Datadog-Bottlecap-Beta-ARM"
+  fi
+  if [ -z $ARCHITECTURE ]; then
+      echo "No architecture specified, defaulting to amd64"
+      ARCHITECTURE="amd64"
+  fi
 
-if [ ! -z "$SUFFIX" ]; then
-   LAYER_NAME+="-$SUFFIX"
-fi
+  if [ ! -z "$SUFFIX" ]; then
+     LAYER_NAME+="-$SUFFIX"
+  fi
 
-if [ -z $REGION ]; then
-    echo "No region specified, defaulting to us-east-1"
-    REGION="us-east-1"
-fi
+  if [ -z $REGION ]; then
+      echo "No region specified, defaulting to us-east-1"
+      REGION="us-east-1"
+  fi
+}
 
-if [ -z $VERSION ]; then
-    echo "No version specified, automatically incrementing version number"
+publish(){
+  # Move into the root directory
+  SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+  cd $SCRIPTS_DIR/..
 
-    LAST_LAYER_VERSION=$(
-        aws-vault exec sso-serverless-sandbox-account-admin -- \
-        aws lambda list-layer-versions \
-            --layer-name $LAYER_NAME \
-            --region $REGION \
-        | jq -r ".LayerVersions | .[0] |  .Version" \
-    )
-    if [ "$LAST_LAYER_VERSION" == "null" ]; then
-        echo "Error auto-detecting the last layer version"
-        exit 1
-    else
-        VERSION=$(($LAST_LAYER_VERSION+1))
-        echo "Will publish new layer version as $VERSION"
-    fi
-fi
+  ARCHITECTURE=$ARCHITECTURE ./scripts/build_bottlecap_layer.sh
+  NEW_VERSION=$(aws-vault exec sso-serverless-sandbox-account-admin -- aws lambda publish-layer-version --layer-name "${LAYER_NAME}" \
+      --description "Datadog Bottlecap Beta" \
+      --zip-file "fileb://${LAYER_PATH}" \
+      --region $REGION | jq -r '.Version')
+  echo "DONE: Published version $NEW_VERSION of layer $LAYER_NAME to region $REGION"
+}
 
-# Move into the root directory
-SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $SCRIPTS_DIR/..
-
-VERSION=$VERSION ARCHITECTURE=$ARCHITECTURE ./scripts/build_bottlecap_layer.sh
-version_nbr=$(aws-vault exec sso-serverless-sandbox-account-admin -- aws lambda publish-layer-version --layer-name "${LAYER_NAME}" \
-    --description "Datadog Bottlecap Beta" \
-    --zip-file "fileb://${LAYER_PATH}" \
-    --region $REGION | jq -r '.Version')
-echo "DONE: Published version $version_nbr of layer $LAYER_NAME to region $REGION"
+_init_arg
+publish
