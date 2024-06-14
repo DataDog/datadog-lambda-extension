7cf1559f90924615c7e075e3d25ee6e7d3c92555
alexgallotta <5581237+alexgallotta@users.noreply.github.com>
2024-06-14 14:23:09 -0400

    set entry and memory limit for metric flush

--- bottlecap/src/metrics/flusher.rs
+++ bottlecap/src/metrics/flusher.rs
@@ -17,11 +17,10 @@ impl Flusher {
 
     pub async fn flush(&mut self) {
         let locked_aggr = &mut self.aggregator.lock().expect("lock poisoned");
-        let current_points = locked_aggr.to_series();
-        let current_distribution_points = locked_aggr.distributions_to_protobuf();
-        if !current_points.series.is_empty() {
-            debug!("flushing {} series to datadog", current_points.series.len());
-            match &self.dd_api.ship_series(&current_points).await {
+        let current_points = locked_aggr.to_series_serialized();
+        if !current_points.is_empty() {
+            debug!("flushing {} series to datadog", current_points.len());
+            match &self.dd_api.ship_series(current_points).await {
                 Ok(()) => {}
                 Err(e) => {
                     debug!("failed to ship metrics to datadog: {:?}", e);
@@ -29,10 +28,11 @@ impl Flusher {
             }
             // TODO(astuyve) retry and do not panic
         }
-        if !current_distribution_points.sketches.is_empty() {
+        let current_distribution_points = locked_aggr.distributions_to_protobuf_serialized();
+        if !current_distribution_points.is_empty() {
             match &self
                 .dd_api
-                .ship_distributions(&current_distribution_points)
+                .ship_distributions(current_distribution_points)
                 .await
             {
                 Ok(()) => {}
