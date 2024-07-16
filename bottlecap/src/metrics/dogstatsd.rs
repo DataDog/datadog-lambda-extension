7875344f55106cf864be5325dfe176502f32f9f7
alexgallotta <5581237+alexgallotta@users.noreply.github.com>
2024-07-16 16:22:07 -0400

    refactor: address comments

--- bottlecap/src/metrics/dogstatsd.rs
+++ bottlecap/src/metrics/dogstatsd.rs
@@ -75,10 +75,7 @@ impl DogStatsD {
             .await
             .expect("didn't receive data");
         let msgs = std::str::from_utf8(&buf).expect("couldn't parse as string");
-        debug!(
-            "received message: {} from {}, sending it to the bus",
-            msgs, src
-        );
+        debug!("received message: {} from {}", msgs, src);
         let statsd_metric_strings = msgs.split('\n');
         self.insert_metrics(statsd_metric_strings);
     }
@@ -88,7 +85,7 @@ impl DogStatsD {
             .filter_map(|m| match Metric::parse(m) {
                 Ok(metric) => Some(metric),
                 Err(e) => {
-                    error!("failed to parse metric: {}", e);
+                    error!("failed to parse metric {}: {}", m, e);
                     None
                 }
             })
@@ -115,32 +112,14 @@ mod tests {
     use std::sync::{Arc, Mutex};
 
     #[tokio::test]
-    async fn test_dogstatsd() {
+    async fn test_dogstatsd_multi_metric() {
         let precision = 0.000_000_01;
-        let tags_provider = Arc::new(Provider::new(
-            Arc::new(Config::default()),
-            LAMBDA_RUNTIME_SLUG.to_string(),
-            &HashMap::default(),
-        ));
-        let aggregator_arc = Arc::new(Mutex::new(
-            Aggregator::new(tags_provider, 1_024).expect("aggregator creation failed"),
-        ));
-        let cancel_token = tokio_util::sync::CancellationToken::new();
+        let locked_aggregator = setup_dogstatsd(
+            "metric1:1|c\nmetric2:2|c|tag2:val2\nmetric3:3|c||tag3:val3,tag4:val4\n",
+        )
+        .await;
+        let mut aggregator = locked_aggregator.lock().expect("lock poisoned");
 
-        let statsd_string =
-            "metric1:1|c\nmetric2:2|c|tag2:val2\nmetric3:3|c||tag3:val3,tag4:val4\n";
-
-        let dogstatsd = DogStatsD {
-            cancel_token,
-            aggregator: Arc::clone(&aggregator_arc),
-            buffer_reader: BufferReader::MirrorReader(
-                statsd_string.as_bytes().to_vec(),
-                SocketAddr::new(IpAddr::V4(Ipv4Addr::new(111, 112, 113, 114)), 0),
-            ),
-        };
-        dogstatsd.consume_statsd().await;
-
-        let mut aggregator = aggregator_arc.lock().expect("lock poisoned");
         let parsed_metrics = aggregator.to_series();
 
         assert_eq!(parsed_metrics.len(), 3);
@@ -166,5 +145,47 @@ mod tests {
             }
             _ => panic!("metric3 not found"),
         }
+    }
+
+    #[tokio::test]
+    async fn test_dogstatsd_single_metric() {
+        let precision = 0.000_000_01;
+        let locked_aggregator = setup_dogstatsd("metric123:99123|c").await;
+        let mut aggregator = locked_aggregator.lock().expect("lock poisoned");
+        let parsed_metrics = aggregator.to_series();
+
+        assert_eq!(parsed_metrics.len(), 1);
+        assert_eq!(aggregator.distributions_to_protobuf().sketches.len(), 0);
+
+        match aggregator.get_value_by_id("metric123".into(), None) {
+            Some(Value(metric)) => {
+                assert!((metric - 99_123.0).abs() < precision);
+            }
+            _ => panic!("metric1 not found"),
+        }
+    }
+
+    async fn setup_dogstatsd(statsd_string: &str) -> Arc<Mutex<Aggregator>> {
+        let tags_provider = Arc::new(Provider::new(
+            Arc::new(Config::default()),
+            LAMBDA_RUNTIME_SLUG.to_string(),
+            &HashMap::default(),
+        ));
+        let aggregator_arc = Arc::new(Mutex::new(
+            Aggregator::new(tags_provider, 1_024).expect("aggregator creation failed"),
+        ));
+        let cancel_token = tokio_util::sync::CancellationToken::new();
+
+        let dogstatsd = DogStatsD {
+            cancel_token,
+            aggregator: Arc::clone(&aggregator_arc),
+            buffer_reader: BufferReader::MirrorReader(
+                statsd_string.as_bytes().to_vec(),
+                SocketAddr::new(IpAddr::V4(Ipv4Addr::new(111, 112, 113, 114)), 0),
+            ),
+        };
+        dogstatsd.consume_statsd().await;
+
+        aggregator_arc
     }
 }
