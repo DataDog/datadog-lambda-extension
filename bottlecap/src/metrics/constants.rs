522f4794c0e6001d531838a9d14b1f44139d9d89
AJ Stuyvenberg <astuyve@gmail.com>
2024-09-03 10:11:02 -0400

    feat: increase to 10k contexts

--- bottlecap/src/metrics/constants.rs
+++ bottlecap/src/metrics/constants.rs
@@ -1,7 +1,7 @@
 /// The maximum tags that a `Metric` may hold.
 pub const MAX_TAGS: usize = 32;
 
-pub const CONTEXTS: usize = 1024;
+pub const CONTEXTS: usize = 10240;
 
 pub static MAX_CONTEXTS: usize = 65_536; // 2**16, arbitrary
 
