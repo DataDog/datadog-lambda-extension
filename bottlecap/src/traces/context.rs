2882600fd348cb861ca8f9789923d3c9492d424b
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2024-09-27 13:28:35 -0400

    add Span/Trace `context`

--- bottlecap/src/traces/context.rs
+++ bottlecap/src/traces/context.rs
@@ -1,14 +1,12 @@
 use std::collections::HashMap;
 
-use datadog_trace_protobuf::pb::SpanLink;
-
-#[derive(Copy, Clone, Default, Debug, PartialEq)]
+#[derive(Copy, Clone, Default, Debug)]
 pub struct Sampling {
     pub priority: Option<i8>,
     pub mechanism: Option<u8>,
 }
 
-#[derive(Clone, Default, Debug, PartialEq)]
+#[derive(Clone, Default, Debug)]
 #[allow(clippy::module_name_repetitions)]
 pub struct SpanContext {
     pub trace_id: u64,
@@ -16,5 +14,4 @@ pub struct SpanContext {
     pub sampling: Option<Sampling>,
     pub origin: Option<String>,
     pub tags: HashMap<String, String>,
-    pub links: Vec<SpanLink>,
 }
