3df6897e4ba1ff2a2c3a2c8f37158452ac91b2c9
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2024-09-27 16:39:25 -0400

    feat(bottlecap): add invocation span (#394)
    * decouple `hyper` from `trace_processor`
    
    * add `handle_traces`
    
    * fix tests
    
    * removed unused import
    
    * move `invocation_context` to `invocation::context` module
    
    also added some more fields and refactored it
    
    * add `new` and `get_sender_copy` to `trace_agent`
    
    * add `get_canonical_resource_name` to `tags_provider`
    
    * add `get_function_name` to `lambda::tags`
    
    * add `MS_TO_NS` constant
    
    * add `invocation::processor`
    
    * update use of `invocation::context`
    
    * make `lifecycle::listener` to use `invocation::processor`
    
    * use `invocation::processor` in `main.rs`
    
    * move `MS_TO_NS` to `invocation::processor`
    
    * remove unnecessary constant
    
    * add `Box::new` back to `trace_agent`
    
    * add some comments
    
    * add unit tests for `context.rs`
    
    * use `on_invocation_start`
    
    * rename `lambda_library_detected` to `tracer_detected`
    
    * fmt
    
    * remove `current_request_id`
    
    I think we dont need it
    
    * add comment
    
    * fmt

--- bottlecap/src/lifecycle/listener.rs
+++ bottlecap/src/lifecycle/listener.rs
@@ -8,9 +8,10 @@ use std::sync::Arc;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{http, Body, Method, Request, Response, StatusCode};
 use serde_json::json;
-use tracing::{error, warn};
+use tokio::sync::Mutex;
+use tracing::{debug, error, warn};
 
-use crate::tags::provider;
+use crate::lifecycle::invocation::processor::Processor as InvocationProcessor;
 
 const HELLO_PATH: &str = "/lambda/hello";
 const START_INVOCATION_PATH: &str = "/lambda/start-invocation";
@@ -18,13 +19,17 @@ const END_INVOCATION_PATH: &str = "/lambda/end-invocation";
 const AGENT_PORT: usize = 8124;
 
 pub struct Listener {
-    pub tags_provider: Arc<provider::Provider>,
+    pub invocation_processor: Arc<Mutex<InvocationProcessor>>,
 }
 
 impl Listener {
     pub async fn start(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let invocation_processor = self.invocation_processor.clone();
+
         let make_svc = make_service_fn(move |_| {
-            let service = service_fn(Self::handler);
+            let invocation_processor = invocation_processor.clone();
+
+            let service = service_fn(move |req| Self::handler(req, invocation_processor.clone()));
 
             async move { Ok::<_, Infallible>(service) }
         });
@@ -44,11 +49,26 @@ impl Listener {
         Ok(())
     }
 
-    #[allow(clippy::unused_async)]
-    async fn handler(req: Request<Body>) -> http::Result<Response<Body>> {
+    async fn handler(
+        req: Request<Body>,
+        invocation_processor: Arc<Mutex<InvocationProcessor>>,
+    ) -> http::Result<Response<Body>> {
         match (req.method(), req.uri().path()) {
-            (&Method::POST, START_INVOCATION_PATH) => Self::start_invocation_handler(req),
-            (&Method::POST, END_INVOCATION_PATH) => Self::end_invocation_handler(req),
+            (&Method::POST, START_INVOCATION_PATH) => {
+                Self::start_invocation_handler(req, invocation_processor).await
+            }
+            (&Method::POST, END_INVOCATION_PATH) => {
+                match Self::end_invocation_handler(req, invocation_processor).await {
+                    Ok(response) => Ok(response),
+                    Err(e) => {
+                        error!("Failed to end invocation {e}");
+                        Ok(Response::builder()
+                            .status(500)
+                            .body(Body::empty())
+                            .expect("no body"))
+                    }
+                }
+            }
             (&Method::GET, HELLO_PATH) => Self::hello_handler(),
             _ => {
                 let mut not_found = Response::default();
@@ -58,13 +78,54 @@ impl Listener {
         }
     }
 
-    fn start_invocation_handler(_: Request<Body>) -> http::Result<Response<Body>> {
+    async fn start_invocation_handler(
+        _: Request<Body>,
+        invocation_processor: Arc<Mutex<InvocationProcessor>>,
+    ) -> http::Result<Response<Body>> {
+        debug!("Received start invocation request");
+        let mut processor = invocation_processor.lock().await;
+        processor.on_invocation_start();
+        drop(processor);
+
         Response::builder()
             .status(200)
             .body(Body::from(json!({}).to_string()))
     }
 
-    fn end_invocation_handler(_: Request<Body>) -> http::Result<Response<Body>> {
+    async fn end_invocation_handler(
+        req: Request<Body>,
+        invocation_processor: Arc<Mutex<InvocationProcessor>>,
+    ) -> http::Result<Response<Body>> {
+        debug!("Received end invocation request");
+        let (parts, _) = req.into_parts();
+        let headers = parts.headers;
+
+        let mut processor = invocation_processor.lock().await;
+
+        let mut trace_id = 0;
+        if let Some(header) = headers.get("x-datadog-trace-id") {
+            if let Ok(header_value) = header.to_str() {
+                trace_id = header_value.parse::<u64>().unwrap_or(0);
+            }
+        }
+
+        let mut span_id = 0;
+        if let Some(header) = headers.get("x-datadog-span-id") {
+            if let Ok(header_value) = header.to_str() {
+                span_id = header_value.parse::<u64>().unwrap_or(0);
+            }
+        }
+
+        let mut parent_id = 0;
+        if let Some(header) = headers.get("x-datadog-parent-id") {
+            if let Ok(header_value) = header.to_str() {
+                parent_id = header_value.parse::<u64>().unwrap_or(0);
+            }
+        }
+
+        processor.on_invocation_end(trace_id, span_id, parent_id);
+        drop(processor);
+
         Response::builder()
             .status(200)
             .body(Body::from(json!({}).to_string()))
