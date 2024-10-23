891d7b2c8d2368a8935a00085a239b5755531cc8
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2024-10-23 13:43:07 -0400

    feat(bottlecap): add Distributed Tracing (#423)
    * headers `HeaderMap` to `HashMap`
    
    * add `Send` to propagators traits
    
    * add `serde_json::Value` extractor + injector
    
    * add `get_carrier` to `Trigger` trait
    
    * add `get_carrier` method to current inferred spans
    
    * update `span_inferrer.rs` to use `get_carrier` methods for distributed tracing
    
    * add `headers_to_map` function
    
    * reparent spans
    
    I suspect there might be something wrong here, the code in Go is quite convoluted
    
    * make some variables public
    
    * fix to return early on `extract_span_context`
    
    * fix how 128 bit is handled
    
    also updated some variable names
    
    * update comment

--- bottlecap/src/traces/propagation/carrier.rs
+++ bottlecap/src/traces/propagation/carrier.rs
@@ -3,6 +3,8 @@
 ///
 use std::collections::HashMap;
 
+use serde_json::Value;
+
 /// Injector provides an interface for a carrier to be used
 /// with a Propagator to inject a Context into the carrier.
 ///
@@ -38,6 +40,35 @@ impl<S: std::hash::BuildHasher> Extractor for HashMap<String, String, S> {
     }
 }
 
+impl Injector for Value {
+    /// Set a key and value in the `Value`.
+    fn set(&mut self, key: &str, value: String) {
+        if let Value::Object(map) = self {
+            map.insert(key.to_lowercase(), Value::String(value));
+        }
+    }
+}
+
+impl Extractor for Value {
+    /// Get a value for a key from the `Value`.
+    fn get(&self, key: &str) -> Option<&str> {
+        if let Value::Object(map) = self {
+            map.get(&key.to_lowercase()).and_then(|v| v.as_str())
+        } else {
+            None
+        }
+    }
+
+    /// Collect all the keys from the `Value`.
+    fn keys(&self) -> Vec<&str> {
+        if let Value::Object(map) = self {
+            map.keys().map(String::as_str).collect::<Vec<_>>()
+        } else {
+            Vec::new()
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -57,6 +88,30 @@ mod test {
     #[test]
     fn hash_map_keys() {
         let mut carrier = HashMap::new();
+        carrier.set("headerName1", "value1".to_string());
+        carrier.set("headerName2", "value2".to_string());
+
+        let got = Extractor::keys(&carrier);
+        assert_eq!(got.len(), 2);
+        assert!(got.contains(&"headername1"));
+        assert!(got.contains(&"headername2"));
+    }
+
+    #[test]
+    fn serde_value_get() {
+        let mut carrier = Value::Object(serde_json::Map::new());
+        carrier.set("headerName", "value".to_string());
+
+        assert_eq!(
+            Extractor::get(&carrier, "HEADERNAME"),
+            Some("value"),
+            "case insensitive extraction"
+        );
+    }
+
+    #[test]
+    fn serde_value_keys() {
+        let mut carrier = Value::Object(serde_json::Map::new());
         carrier.set("headerName1", "value1".to_string());
         carrier.set("headerName2", "value2".to_string());
 
