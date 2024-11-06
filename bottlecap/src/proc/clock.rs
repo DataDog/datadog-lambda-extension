839d76f8f8a150d9787691b285655e07c74b39d3
shreyamalpani <shreya.malpani@datadoghq.com>
2024-11-06 11:26:35 -0500

    Generate CPU Enhanced Metrics (#430)
    * send cpu metrics
    
    * clippy fixes
    
    * fixes
    
    * set utilization metrics before flushing & format fixes
    
    * added comment to explain utilization metrics calculation timing
    
    * use nix instead of libc to get system clock
    
    * update LICENSE-3rdparty.yml
    
    * added comments to explain calculations
    
    * clippy

---
+++ bottlecap/src/proc/clock.rs
@@ -0,0 +1,20 @@
+use nix::unistd::{sysconf, SysconfVar};
+use std::io;
+
+#[allow(clippy::cast_sign_loss)]
+#[cfg(not(target_os = "windows"))]
+pub fn get_clk_tck() -> Result<u64, io::Error> {
+    match sysconf(SysconfVar::CLK_TCK) {
+        Ok(Some(clk_tck)) if clk_tck > 0 => Ok(clk_tck as u64),
+        _ => Err(io::Error::new(
+            io::ErrorKind::NotFound,
+            "Could not find system clock ticks per second",
+        )),
+    }
+}
+
+#[cfg(target_os = "windows")]
+pub fn get_clk_tck() -> Result<u64, io::Error> {
+    // Windows does not have this concept
+    Ok(1)
+}
--- bottlecap/src/proc/constants.rs
+++ bottlecap/src/proc/constants.rs
@@ -1,3 +1,5 @@
 pub const PROC_NET_DEV_PATH: &str = "/proc/net/dev";
+pub const PROC_STAT_PATH: &str = "/proc/stat";
+pub const PROC_UPTIME_PATH: &str = "/proc/uptime";
 
 pub const LAMDBA_NETWORK_INTERFACE: &str = "vinternal_1";
