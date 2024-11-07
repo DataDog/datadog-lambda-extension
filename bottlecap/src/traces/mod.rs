395af153bd351ae9443206ac8256afb9708e1c71
alexgallotta <5581237+alexgallotta@users.noreply.github.com>
2024-11-07 15:38:03 -0500

    update comments

--- bottlecap/src/traces/mod.rs
+++ bottlecap/src/traces/mod.rs
@@ -9,22 +9,23 @@ pub mod trace_agent;
 pub mod trace_flusher;
 pub mod trace_processor;
 
-// lambdaRuntimeUrlPrefix is the first part of a URL for a call to the Lambda runtime API. The value may be replaced if `AWS_LAMBDA_RUNTIME_API` is set.
+// the first part of a URL for a call to the Lambda runtime API. The value may be replaced if `AWS_LAMBDA_RUNTIME_API` is set.
 const LAMBDA_RUNTIME_URL_PREFIX: &str = "http://127.0.0.1:9001";
 
-// lambdaExtensionURLPrefix is the first part of a URL for a call from the Datadog Lambda Library to the Lambda Extension
+// the first part of a URL for a call from the Datadog Lambda Library to the Lambda Extension
 const LAMBDA_EXTENSION_URL_PREFIX: &str = "http://127.0.0.1:8124";
 
-// lambdaStatsDURLPrefix is the first part of a URL for a call from Statsd
+// the first part of a URL for a call from Statsd
 const LAMBDA_STATSD_URL_PREFIX: &str = "http://127.0.0.1:8125";
 
-// dnsNonRoutableAddressURLPrefix is the first part of a URL from the non-routable address for DNS traces
+// the first part of a URL from the non-routable address for DNS traces
 const DNS_NON_ROUTABLE_ADDRESS_URL_PREFIX: &str = "0.0.0.0";
 
-// dnsLocalHostAddressURLPrefix is the first part of a URL from the localhost address for DNS traces
+// the first part of a URL from the localhost address for DNS traces
 const DNS_LOCAL_HOST_ADDRESS_URL_PREFIX: &str = "127.0.0.1";
 
 // awsXrayDaemonAddressURLPrefix is the first part of a URL from the _AWS_XRAY_DAEMON_ADDRESS for DNS traces
 const AWS_XRAY_DAEMON_ADDRESS_URL_PREFIX: &str = "169.254.79.129";
 
+// name of the placeholder span for java and golang tracer
 const INVOCATION_SPAN_RESOURCE: &str = "dd-tracer-serverless-span";
