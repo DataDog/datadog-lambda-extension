16db49665b4fc69b3ddc62568cd5ec39c1f82f1f
AJ Stuyvenberg <astuyve@gmail.com>
2025-01-24 10:12:07 -0500

    feat: Support nonstandard proxy config

--- bottlecap/src/config/mod.rs
+++ bottlecap/src/config/mod.rs
@@ -14,7 +14,6 @@ use figment::{providers::Env, Figment};
 use serde::{Deserialize, Deserializer};
 use serde_json::Value;
 use trace_propagation_style::{deserialize_trace_propagation_style, TracePropagationStyle};
-use tracing::debug;
 
 use crate::config::{
     flush_strategy::FlushStrategy,
@@ -81,7 +80,7 @@ pub struct YamlConfig {
 #[allow(clippy::module_name_repetitions)]
 pub struct YamlProxyConfig {
     pub https: Option<String>,
-    pub no_proxy: Option<String>,
+    pub no_proxy: Option<Vec<String>>,
 }
 
 #[derive(Debug, PartialEq, Deserialize, Clone)]
@@ -263,20 +262,29 @@ pub fn get_config(config_directory: &Path) -> Result<Config, ConfigError> {
 
     // Prefer DD_PROXY_HTTPS over HTTPS_PROXY
     // No else needed as HTTPS_PROXY is handled by reqwest and built into trace client
-    if let Ok(https_proxy) = std::env::var("DD_PROXY_HTTPS") {
-        config.https_proxy = Some(https_proxy);
+    if let Ok(https_proxy) = std::env::var("DD_PROXY_HTTPS").or_else(|_| {
+        yaml_config
+            .proxy
+            .https
+            .clone()
+            .ok_or(std::env::VarError::NotPresent)
+    }) {
+        if std::env::var("NO_PROXY").map_or(false, |no_proxy| no_proxy.contains(&config.site))
+            || yaml_config
+                .proxy
+                .no_proxy
+                .map_or(false, |no_proxy| no_proxy.contains(&config.site))
+        {
+            config.https_proxy = None;
+        } else {
+            config.https_proxy = Some(https_proxy);
+        }
     }
     // Set site if empty
     if config.site.is_empty() {
         config.site = "datadoghq.com".to_string();
     }
-    // TODO(astuyve)
-    // Bit of a hack as we're not checking individual privatelink setups
-    // potentially a user could use the proxy for logs and privatelink for APM
-    if std::env::var("NO_PROXY").map_or(false, |no_proxy| no_proxy.contains(&config.site)) {
-        debug!("NO_PROXY contains DD_SITE, disabling proxy");
-        config.https_proxy = None;
-    }
+
     // Merge YAML nested fields
     //
     // Set logs_config_processing_rules if not defined in env
