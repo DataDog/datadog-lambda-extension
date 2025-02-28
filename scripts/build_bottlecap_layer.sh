9e7ea9f52990d3e25a8819760a124e83887f5d5e
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2025-02-28 13:31:11 -0500

    pass `FILE_SUFFIX` to build

--- scripts/build_bottlecap_layer.sh
+++ scripts/build_bottlecap_layer.sh
@@ -38,7 +38,7 @@ rm -rf ${EXTENSION_PATH} 2>/dev/null
 cd $ROOT_DIR
 
 ALPINE=0
-ARCHITECTURE=$ARCHITECTURE ALPINE=$ALPINE .gitlab/scripts/compile_bottlecap.sh
+ARCHITECTURE=$ARCHITECTURE ALPINE=$ALPINE FILE_SUFFIX=$FILE_SUFFIX .gitlab/scripts/compile_bottlecap.sh
 
 docker buildx build --platform linux/${ARCHITECTURE} \
     -t datadog/build-bottlecap-${FILE_SUFFIX} \
