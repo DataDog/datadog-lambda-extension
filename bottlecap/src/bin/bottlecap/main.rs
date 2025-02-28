8e3ee20d6c1eb3800871bbdc07fefd14e3e3bd88
alexgallotta <5581237+alexgallotta@users.noreply.github.com>
2025-02-28 11:20:44 -0500

    use info log level for startup message

--- bottlecap/src/bin/bottlecap/main.rs
+++ bottlecap/src/bin/bottlecap/main.rs
@@ -68,7 +68,7 @@ use std::{
 use telemetry::listener::TelemetryListenerConfig;
 use tokio::{sync::mpsc::Sender, sync::Mutex as TokioMutex};
 use tokio_util::sync::CancellationToken;
-use tracing::{debug, error};
+use tracing::{debug, error, info};
 use tracing_subscriber::EnvFilter;
 
 #[derive(Clone, Deserialize)]
@@ -183,11 +183,11 @@ fn build_function_arn(account_id: &str, region: &str, function_name: &str) -> St
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let version_without_next = EXTENSION_VERSION.split('-').next().unwrap_or("NA");
-    println!("DD_EXTENSION | Starting Datadog Extension {version_without_next}");
     let (mut aws_config, config) = load_configs();
 
     enable_logging_subsystem(&config);
+    let version_without_next = EXTENSION_VERSION.split('-').next().unwrap_or("NA");
+    info!("DD_EXTENSION | Starting Datadog Extension {version_without_next}");
     let client = Client::builder().no_proxy().build().map_err(|e| {
         Error::new(
             std::io::ErrorKind::InvalidData,
