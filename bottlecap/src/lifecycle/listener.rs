b7bb6b339fe9ee1dda644a257fc1477994601e6d
alexgallotta <5581237+alexgallotta@users.noreply.github.com>
2025-03-13 11:25:55 -0400

    update code after merge and use start/end lifecycle endpoints with whole body and headers

--- bottlecap/src/lifecycle/listener.rs
+++ bottlecap/src/lifecycle/listener.rs
@@ -7,13 +7,14 @@ use std::net::SocketAddr;
 use std::sync::Arc;
 
 use hyper::body::HttpBody;
+use hyper::http::request::Parts;
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{http, Body, Method, Request, Response, StatusCode};
+use hyper::{http, Body, HeaderMap, Method, Request, Response, StatusCode};
 use serde_json::json;
 use tokio::sync::Mutex;
 use tracing::{debug, error, warn};
 
-use crate::lifecycle::invocation::processor::Processor as InvocationProcessor;
+use crate::lifecycle::invocation::processor::{Processor as InvocationProcessor, Processor};
 use crate::traces::propagation::text_map_propagator::{
     DATADOG_HIGHER_ORDER_TRACE_ID_BITS_KEY, DATADOG_SAMPLING_PRIORITY_KEY, DATADOG_TAGS_KEY,
     DATADOG_TRACE_ID_KEY,
@@ -61,7 +62,8 @@ impl Listener {
     ) -> http::Result<Response<Body>> {
         match (req.method(), req.uri().path()) {
             (&Method::POST, START_INVOCATION_PATH) => {
-                Self::start_invocation_handler(req, invocation_processor).await
+                let (parts, body) = req.into_parts();
+                Self::start_invocation_handler(parts.headers, body, invocation_processor).await
             }
             (&Method::POST, END_INVOCATION_PATH) => {
                 match Self::end_invocation_handler(req, invocation_processor).await {
@@ -84,18 +86,18 @@ impl Listener {
         }
     }
 
-    async fn start_invocation_handler(
-        req: Request<Body>,
+    pub async fn start_invocation_handler(
+        headers: HeaderMap,
+        body: Body,
         invocation_processor: Arc<Mutex<InvocationProcessor>>,
     ) -> http::Result<Response<Body>> {
         debug!("Received start invocation request");
-        let (parts, body) = req.into_parts();
         match body.collect().await {
             Ok(b) => {
                 let body = b.to_bytes().to_vec();
                 let mut processor = invocation_processor.lock().await;
 
-                let headers = Self::headers_to_map(parts.headers);
+                let headers = Self::headers_to_map(headers);
 
                 processor.on_invocation_start(headers, body);
 
@@ -136,7 +138,7 @@ impl Listener {
         }
     }
 
-    async fn end_invocation_handler(
+    pub async fn end_invocation_handler(
         req: Request<Body>,
         invocation_processor: Arc<Mutex<InvocationProcessor>>,
     ) -> http::Result<Response<Body>> {
