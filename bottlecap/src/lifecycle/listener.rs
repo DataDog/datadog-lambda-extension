d33d1648b3bcb1387db912137f80af436ecd4258
alexgallotta <5581237+alexgallotta@users.noreply.github.com>
2025-03-13 15:56:56 -0400

    add end invocation call

--- bottlecap/src/lifecycle/listener.rs
+++ bottlecap/src/lifecycle/listener.rs
@@ -7,14 +7,13 @@ use std::net::SocketAddr;
 use std::sync::Arc;
 
 use hyper::body::HttpBody;
-use hyper::http::request::Parts;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{http, Body, HeaderMap, Method, Request, Response, StatusCode};
 use serde_json::json;
 use tokio::sync::Mutex;
 use tracing::{debug, error, warn};
 
-use crate::lifecycle::invocation::processor::{Processor as InvocationProcessor, Processor};
+use crate::lifecycle::invocation::processor::{Processor as InvocationProcessor};
 use crate::traces::propagation::text_map_propagator::{
     DATADOG_HIGHER_ORDER_TRACE_ID_BITS_KEY, DATADOG_SAMPLING_PRIORITY_KEY, DATADOG_TAGS_KEY,
     DATADOG_TRACE_ID_KEY,
@@ -66,7 +65,8 @@ impl Listener {
                 Self::start_invocation_handler(parts.headers, body, invocation_processor).await
             }
             (&Method::POST, END_INVOCATION_PATH) => {
-                match Self::end_invocation_handler(req, invocation_processor).await {
+                let (parts, body) = req.into_parts();
+                match Self::end_invocation_handler(parts.headers, body, invocation_processor).await {
                     Ok(response) => Ok(response),
                     Err(e) => {
                         error!("Failed to end invocation {e}");
@@ -139,17 +139,17 @@ impl Listener {
     }
 
     pub async fn end_invocation_handler(
-        req: Request<Body>,
+        headers: HeaderMap,
+        body: Body,
         invocation_processor: Arc<Mutex<InvocationProcessor>>,
     ) -> http::Result<Response<Body>> {
         debug!("Received end invocation request");
-        let (parts, body) = req.into_parts();
         match body.collect().await {
             Ok(b) => {
                 let body = b.to_bytes().to_vec();
                 let mut processor = invocation_processor.lock().await;
 
-                let headers = Self::headers_to_map(parts.headers);
+                let headers = Self::headers_to_map(headers);
                 processor.on_invocation_end(headers, body);
                 drop(processor);
 
