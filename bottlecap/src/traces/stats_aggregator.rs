561f100ab1f5f3f8e9ac5cbdfcfce60a112b6e1d
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2025-04-15 17:22:56 -0400

    use `size_of_val` for payload, instead of `.encoded_len()`
    method no longer works when updating dependencies so I had to modify this

--- bottlecap/src/traces/stats_aggregator.rs
+++ bottlecap/src/traces/stats_aggregator.rs
@@ -1,5 +1,4 @@
 use datadog_trace_protobuf::pb::ClientStatsPayload;
-use prost::Message;
 use std::collections::VecDeque;
 
 /// Maximum number of entries in a stat payload.
@@ -55,7 +54,7 @@ impl StatsAggregator {
         // Fill the batch
         while batch_size < self.max_content_size_bytes {
             if let Some(payload) = self.queue.pop_front() {
-                let payload_size = payload.encoded_len();
+                let payload_size = size_of_val(&payload);
 
                 // Put stats back in the queue
                 if batch_size + payload_size > self.max_content_size_bytes {
