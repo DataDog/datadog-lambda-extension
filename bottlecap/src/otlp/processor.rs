4de5910712f8c6e36946624e12358a972d71717b
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2025-04-15 17:19:32 -0400

    add processor
    uses opentelemetry tonic protobuf parsing required for proper deserialization for further translation to datadog protocol

---
+++ bottlecap/src/otlp/processor.rs
@@ -0,0 +1,33 @@
+use datadog_trace_protobuf::pb::Span as DatadogSpan;
+use opentelemetry_proto::tonic::collector::trace::v1::ExportTraceServiceRequest;
+use prost::Message;
+use std::{error::Error, sync::Arc};
+
+use crate::{config::Config, otlp::transform::otel_resource_spans_to_dd_spans};
+
+#[derive(Clone)]
+pub struct Processor {
+    config: Arc<Config>,
+}
+
+impl Processor {
+    #[must_use]
+    pub fn new(config: Arc<Config>) -> Self {
+        Self { config }
+    }
+
+    pub fn process(&self, body: &[u8]) -> Result<Vec<Vec<DatadogSpan>>, Box<dyn Error>> {
+        // Decode the OTLP HTTP request
+        let request = ExportTraceServiceRequest::decode(body)?;
+
+        let mut spans: Vec<Vec<DatadogSpan>> = Vec::new();
+        for resource_spans in &request.resource_spans {
+            spans.extend(otel_resource_spans_to_dd_spans(
+                resource_spans,
+                self.config.clone(),
+            ));
+        }
+
+        Ok(spans)
+    }
+}
