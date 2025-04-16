9b58f3917e64de6b4e68c9d69c2f13f3cb4868c4
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2025-04-16 16:22:00 -0400

    chore(config): parse config only twice  (#651)
    # What?
    
    Removes `FallbackConfig` and `FallbackYamlConfig` in favor of the
    existing configurations.
    
    # How?
    
    1. Using only the known places where we are going to fallback from the
    available configs.
    2. Moved environment variables and yaml config to its own file for
    readability.
    
    # Notes
    
    - Added fallbacks for OTLP (in preparation for that PR, allowed some
    fields to not fallback).

---
+++ bottlecap/src/config/yaml.rs
@@ -0,0 +1,120 @@
+use std::collections::HashMap;
+
+use crate::config::{deserialize_apm_replace_rules, deserialize_processing_rules, ProcessingRule};
+use datadog_trace_obfuscation::replacer::ReplaceRule;
+use serde::Deserialize;
+use serde_json::Value;
+
+/// `Config` is a struct that represents some of the fields in the `datadog.yaml` file.
+///
+/// It is used to deserialize the `datadog.yaml` file into a struct that can be merged with the `Config` struct.
+#[derive(Debug, PartialEq, Deserialize, Clone, Default)]
+#[serde(default)]
+#[allow(clippy::module_name_repetitions)]
+pub struct Config {
+    pub logs_config: LogsConfig,
+    pub apm_config: ApmConfig,
+    pub proxy: ProxyConfig,
+    pub otlp_config: OtlpConfig,
+}
+
+/// Logs Config
+///
+
+#[derive(Debug, PartialEq, Deserialize, Clone, Default)]
+#[serde(default)]
+#[allow(clippy::module_name_repetitions)]
+pub struct LogsConfig {
+    #[serde(deserialize_with = "deserialize_processing_rules")]
+    pub processing_rules: Option<Vec<ProcessingRule>>,
+}
+
+/// APM Config
+///
+
+#[derive(Debug, PartialEq, Deserialize, Clone, Default)]
+#[serde(default)]
+#[allow(clippy::module_name_repetitions)]
+pub struct ApmConfig {
+    #[serde(deserialize_with = "deserialize_apm_replace_rules")]
+    pub replace_tags: Option<Vec<ReplaceRule>>,
+    pub obfuscation: Option<ApmObfuscation>,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Clone, Copy, Default)]
+#[serde(default)]
+#[allow(clippy::module_name_repetitions)]
+pub struct ApmObfuscation {
+    pub http: ApmHttpObfuscation,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Clone, Copy, Default)]
+#[serde(default)]
+#[allow(clippy::module_name_repetitions)]
+pub struct ApmHttpObfuscation {
+    pub remove_query_string: bool,
+    pub remove_paths_with_digits: bool,
+}
+
+/// Proxy Config
+///
+
+#[derive(Debug, PartialEq, Deserialize, Clone, Default)]
+#[serde(default)]
+#[allow(clippy::module_name_repetitions)]
+pub struct ProxyConfig {
+    pub https: Option<String>,
+    pub no_proxy: Option<Vec<String>>,
+}
+
+/// OTLP Config
+///
+
+#[derive(Debug, PartialEq, Deserialize, Clone, Default)]
+#[serde(default)]
+#[allow(clippy::module_name_repetitions)]
+pub struct OtlpConfig {
+    pub receiver: OtlpReceiverConfig,
+    pub traces: OtlpTracesConfig,
+
+    // NOT SUPPORTED
+    pub metrics: Option<Value>,
+    pub logs: Option<Value>,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Clone, Default)]
+#[serde(default)]
+#[allow(clippy::module_name_repetitions)]
+pub struct OtlpReceiverConfig {
+    pub protocols: OtlpReceiverProtocolsConfig,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Clone, Default)]
+#[serde(default)]
+#[allow(clippy::module_name_repetitions)]
+pub struct OtlpReceiverProtocolsConfig {
+    pub http: OtlpReceiverHttpConfig,
+
+    // NOT SUPPORTED
+    pub grpc: Option<Value>,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Clone, Default)]
+#[serde(default)]
+#[allow(clippy::module_name_repetitions)]
+pub struct OtlpReceiverHttpConfig {
+    pub endpoint: String,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Clone, Default)]
+#[serde(default)]
+#[allow(clippy::module_name_repetitions)]
+pub struct OtlpTracesConfig {
+    pub enabled: bool,
+    pub span_name_as_resource_name: bool,
+    pub span_name_remappings: HashMap<String, String>,
+    pub ignore_missing_datadog_fields: bool,
+
+    // NOT SUPORTED
+    pub probabilistic_sampler: Option<Value>,
+}
