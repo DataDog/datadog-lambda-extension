adebc2cb6b226050710109e93a6e0175161a6cb2
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2025-05-02 18:33:01 -0400

    add `should_start_proxy` method
    allows us to have clear control of when we enable the proxy

--- bottlecap/src/proxy/mod.rs
+++ bottlecap/src/proxy/mod.rs
@@ -1,1 +1,142 @@
+use std::sync::Arc;
+
+use crate::config::{aws::AwsConfig, Config};
+
 pub mod interceptor;
+
+/// Returns true if the proxy should be started.
+///
+/// The proxy should be started if:
+/// - The LWA proxy is set, or
+/// - ASM is enabled and the `AWS_LAMBDA_EXEC_WRAPPER` environment variable is set to `/opt/datadog_wrapper`
+#[must_use]
+#[allow(clippy::module_name_repetitions)]
+pub fn should_start_proxy(config: &Arc<Config>, aws_config: &AwsConfig) -> bool {
+    let lwa_proxy_set = aws_config.aws_lwa_proxy_lambda_runtime_api.is_some();
+    let datadog_wrapper_set = aws_config
+        .exec_wrapper
+        .as_ref()
+        .is_some_and(|s| s.eq("/opt/datadog_wrapper"));
+    let appsec_enabled = config.appsec_enabled || config.serverless_appsec_enabled;
+
+    lwa_proxy_set || (datadog_wrapper_set && appsec_enabled)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::time::Instant;
+
+    use super::*;
+
+    #[test]
+    fn test_should_start_proxy_everything_set() {
+        let config = Arc::new(Config {
+            // Appsec is enabled, so we should start the proxy
+            appsec_enabled: true,
+            ..Default::default()
+        });
+        let aws_config = AwsConfig {
+            region: "us-east-1".to_string(),
+            aws_access_key_id: "".to_string(),
+            aws_secret_access_key: "".to_string(),
+            aws_session_token: "".to_string(),
+            aws_container_credentials_full_uri: "".to_string(),
+            aws_container_authorization_token: "".to_string(),
+            aws_lwa_proxy_lambda_runtime_api: Some("127.0.0.1:12345".to_string()),
+            function_name: "".to_string(),
+            runtime_api: "".to_string(),
+            sandbox_init_time: Instant::now(),
+            exec_wrapper: Some("/opt/datadog_wrapper".to_string()),
+        };
+        assert!(should_start_proxy(&config, &aws_config));
+    }
+    #[test]
+    fn test_should_start_proxy_lwa_proxy_set() {
+        let config = Arc::new(Config::default());
+        let aws_config = AwsConfig {
+            region: "us-east-1".to_string(),
+            aws_access_key_id: "".to_string(),
+            aws_secret_access_key: "".to_string(),
+            aws_session_token: "".to_string(),
+            aws_container_credentials_full_uri: "".to_string(),
+            aws_container_authorization_token: "".to_string(),
+            // LWA proxy is set, so we should start the proxy
+            aws_lwa_proxy_lambda_runtime_api: Some("127.0.0.1:12345".to_string()),
+            function_name: "".to_string(),
+            runtime_api: "".to_string(),
+            sandbox_init_time: Instant::now(),
+            exec_wrapper: None,
+        };
+        assert!(should_start_proxy(&config, &aws_config));
+    }
+
+    #[test]
+    fn test_should_start_proxy_appsec_enabled_and_datadog_wrapper_set() {
+        let config = Arc::new(Config {
+            // Appsec is enabled, so we should start the proxy
+            appsec_enabled: true,
+            ..Default::default()
+        });
+        let aws_config = AwsConfig {
+            region: "us-east-1".to_string(),
+            aws_access_key_id: "".to_string(),
+            aws_secret_access_key: "".to_string(),
+            aws_session_token: "".to_string(),
+            aws_container_credentials_full_uri: "".to_string(),
+            aws_container_authorization_token: "".to_string(),
+            aws_lwa_proxy_lambda_runtime_api: None,
+            function_name: "".to_string(),
+            runtime_api: "".to_string(),
+            sandbox_init_time: Instant::now(),
+            exec_wrapper: Some("/opt/datadog_wrapper".to_string()),
+        };
+        assert!(should_start_proxy(&config, &aws_config));
+    }
+
+    #[test]
+    fn test_should_start_proxy_appsec_disabled_and_datadog_wrapper_set() {
+        let config = Arc::new(Config {
+            // Appsec is disabled, so we should not start the proxy
+            appsec_enabled: false,
+            ..Default::default()
+        });
+        let aws_config = AwsConfig {
+            region: "us-east-1".to_string(),
+            aws_access_key_id: "".to_string(),
+            aws_secret_access_key: "".to_string(),
+            aws_session_token: "".to_string(),
+            aws_container_credentials_full_uri: "".to_string(),
+            aws_container_authorization_token: "".to_string(),
+            aws_lwa_proxy_lambda_runtime_api: None,
+            function_name: "".to_string(),
+            runtime_api: "".to_string(),
+            sandbox_init_time: Instant::now(),
+            exec_wrapper: Some("/opt/datadog_wrapper".to_string()),
+        };
+        assert!(!should_start_proxy(&config, &aws_config));
+    }
+
+    #[test]
+    fn test_should_start_proxy_appsec_enabled_datadog_wrapper_not_set() {
+        let config = Arc::new(Config {
+            // Appsec is enabled, so we should not start the proxy
+            appsec_enabled: true,
+            ..Default::default()
+        });
+        let aws_config = AwsConfig {
+            region: "us-east-1".to_string(),
+            aws_access_key_id: "".to_string(),
+            aws_secret_access_key: "".to_string(),
+            aws_session_token: "".to_string(),
+            aws_container_credentials_full_uri: "".to_string(),
+            aws_container_authorization_token: "".to_string(),
+            aws_lwa_proxy_lambda_runtime_api: None,
+            function_name: "".to_string(),
+            runtime_api: "".to_string(),
+            sandbox_init_time: Instant::now(),
+            // Datadog wrapper is not set, so we should not start the proxy
+            exec_wrapper: Some("/opt/not_datadog".to_string()),
+        };
+        assert!(!should_start_proxy(&config, &aws_config));
+    }
+}
