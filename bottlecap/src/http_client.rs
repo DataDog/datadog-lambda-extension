2174ba99adee72153695b1984feb857b3ab409b7
AJ Stuyvenberg <astuyve@gmail.com>
2025-05-27 14:19:08 -0400

    fix: httpmock doesn't support http2 yet so we need to force http1 for integration tests

--- bottlecap/src/http_client.rs
+++ bottlecap/src/http_client.rs
@@ -18,22 +18,22 @@ pub fn get_client(config: Arc<config::Config>) -> reqwest::Client {
 }
 
 fn build_client(config: Arc<config::Config>) -> Result<reqwest::Client, Box<dyn Error>> {
+    #[cfg(not(feature = "integration_test"))]
     let client = create_reqwest_client_builder()?
         .timeout(Duration::from_secs(config.flush_timeout))
-        // Temporarily not force http2
         // Enable HTTP/2 for better multiplexing
         .http2_prior_knowledge()
         .http2_keep_alive_interval(Some(Duration::from_secs(10)))
         .http2_keep_alive_while_idle(true)
         .http2_keep_alive_timeout(Duration::from_secs(1000))
-        //.http2_initial_stream_window_size(5_000_000) // magic number
-        //.http2_initial_connection_window_size(5_000_000)
         // Set keep-alive timeout
         .pool_idle_timeout(Some(Duration::from_secs(270)))
-        // Set maximum idle connections per host
-        //.pool_max_idle_per_host(8)
         // Enable TCP keepalive
         .tcp_keepalive(Some(Duration::from_secs(120)));
+
+    #[cfg(feature = "integration_test")]
+    let client = create_reqwest_client_builder();
+
     // This covers DD_PROXY_HTTPS and HTTPS_PROXY
     if let Some(https_uri) = &config.https_proxy {
         let proxy = reqwest::Proxy::https(https_uri.clone())?;
