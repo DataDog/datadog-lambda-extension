a2548ec8a28fa57e2f78549d823da4814872b0cd
Duncan Harvey <duncan.harvey@datadoghq.com>
2025-07-16 14:34:14 -0400

    add status code and logs for troubleshooting

--- scripts/serverless_init_dotnet.sh
+++ scripts/serverless_init_dotnet.sh
@@ -6,28 +6,35 @@ if [ -f "/etc/alpine-release" ]; then
     exit 1
 fi
 
-# Make sure curl is installed
-apt-get update && apt-get install -y curl
+# Make sure curl and jq are installed
+apt-get update && apt-get install -y curl jq
 
 ghcurl() {
   if [ -n "$GITHUB_TOKEN" ]; then
-    echo "Github token provided"
-    curl -sSL -H "Authorization: Bearer $GITHUB_TOKEN" "$@"
+    echo "Github token provided" >&2
+    curl -sSL -w '{"status_code": %{http_code}}' -H "Authorization: Bearer $GITHUB_TOKEN" "$@" | jq -sre add
   else
-    echo "Github token not provided"
-    curl -sSL "$@"
+    echo "Github token not provided" >&2
+    curl -sSL -w '{"status_code": %{http_code}}' "$@" | jq -sre add
   fi
 }
 
 # Get latest released version of dd-trace-dotnet
-TRACER_VERSION=$(ghcurl https://api.github.com/repos/DataDog/dd-trace-dotnet/releases/latest |
-    grep 'tag_name' | cut -d '"' -f 4)
-TRACER_VERSION=${TRACER_VERSION#v} # remove the 'v' prefix
+response=$(ghcurl https://api.github.com/repos/DataDog/dd-trace-dotnet/releases/latest)
+sanitized_response=$(echo "$response" | tr -d '\000-\037') # remove control characters from json response
+echo "Status code of version request: $(echo "$sanitized_response" | jq '.status_code')"
+TRACER_VERSION=$(echo "$sanitized_response" | jq -r '.tag_name // empty'  | sed 's/^v//')
+
+if [ -z "$TRACER_VERSION" ]; then
+  echo "Error: Could not determine the tracer version. Exiting." >&2
+  exit 1
+fi
 
 # Download the tracer to the dd_tracer folder
 echo Downloading version "${TRACER_VERSION}" of the .NET tracer into /tmp/datadog-dotnet-apm.tar.gz
-ghcurl "https://github.com/DataDog/dd-trace-dotnet/releases/download/v${TRACER_VERSION}/datadog-dotnet-apm-${TRACER_VERSION}.tar.gz" \
-    -o /tmp/datadog-dotnet-apm.tar.gz
+response=$(ghcurl "https://github.com/DataDog/dd-trace-dotnet/releases/download/v${TRACER_VERSION}/datadog-dotnet-apm-${TRACER_VERSION}.tar.gz" \
+    -o /tmp/datadog-dotnet-apm.tar.gz)
+echo "Status code of download request: $(echo "$response" | jq '.status_code')"
 
 # Unarchive the tracer and remove the tmp
 mkdir -p /dd_tracer/dotnet
