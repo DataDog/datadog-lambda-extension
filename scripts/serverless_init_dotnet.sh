387596572bdb1ba804d7c39039392ef91878a400
Duncan Harvey <duncan.harvey@datadoghq.com>
2025-07-16 10:13:51 -0400

    use token if available for all requests to github

--- scripts/serverless_init_dotnet.sh
+++ scripts/serverless_init_dotnet.sh
@@ -9,14 +9,24 @@ fi
 # Make sure curl is installed
 apt-get update && apt-get install -y curl
 
+ghcurl() {
+  if [ -n "$GITHUB_TOKEN" ]; then
+    echo "Github token provided"
+    curl -sSL -H "Authorization: Bearer $GITHUB_TOKEN" "$@"
+  else
+    echo "Github token not provided"
+    curl -sSL "$@"
+  fi
+}
+
 # Get latest released version of dd-trace-dotnet
-TRACER_VERSION=$(curl -s https://api.github.com/repos/DataDog/dd-trace-dotnet/releases/latest | grep 'tag_name' | cut -d '"' -f 4)
+TRACER_VERSION=$(ghcurl https://api.github.com/repos/DataDog/dd-trace-dotnet/releases/latest |
+    grep 'tag_name' | cut -d '"' -f 4)
 TRACER_VERSION=${TRACER_VERSION#v} # remove the 'v' prefix
 
 # Download the tracer to the dd_tracer folder
 echo Downloading version "${TRACER_VERSION}" of the .NET tracer into /tmp/datadog-dotnet-apm.tar.gz
-curl -L -H "Authorization: Bearer $GITHUB_TOKEN" \
-    "https://github.com/DataDog/dd-trace-dotnet/releases/download/v${TRACER_VERSION}/datadog-dotnet-apm-${TRACER_VERSION}.tar.gz" \
+ghcurl "https://github.com/DataDog/dd-trace-dotnet/releases/download/v${TRACER_VERSION}/datadog-dotnet-apm-${TRACER_VERSION}.tar.gz" \
     -o /tmp/datadog-dotnet-apm.tar.gz
 
 # Unarchive the tracer and remove the tmp
