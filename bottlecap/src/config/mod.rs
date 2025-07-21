48f4e4089186540618b0b70b9b69e69cd94cddbf
Yiming Luo <yiming.luo@datadoghq.com>
2025-07-21 11:45:09 -0400

    chore: Add doc and rename function for flushing strategy (#740)
    # Motivation
    
    It took me quite some effort to understand flushing strategies. I want
    to make it easier to understand for me and future developers.
    
    # This PR
    Tries to make flushing strategy code more readable:
    1. Add/move comments
    2. Create an enum `ConcreteFlushStrategy`, which doesn't contain
    `Default` because it is required to be resolved to a concrete strategy
    3. Rename `should_adapt` to `evaluate_concrete_strategy()`
    
    # To reviewers
    There are still a few things I don't understand, which are marked with
    `TODO`. Appreciate explanation!
    Also correct me if any comment I added is wrong.

--- bottlecap/src/config/mod.rs
+++ bottlecap/src/config/mod.rs
@@ -240,6 +240,7 @@ pub struct Config {
     pub api_key: String,
     pub log_level: LogLevel,
 
+    // Timeout for the request to flush data to Datadog endpoint
     pub flush_timeout: u64,
 
     // Proxy
