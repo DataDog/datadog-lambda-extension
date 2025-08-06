75ab348c7c470ad72e434adbc9a60d4b1fae679d
Yiming Luo <yiming.luo@datadoghq.com>
2025-08-06 17:19:06 -0400

    fix: Fix parsing provided.al runtime by reading /etc/os-release (#778)
    # Problem
    We fail to parse the runtime when it is `provided.al2` or
    `provided.al2023`. I don't though what's the impact though, because in
    Datadog logs, the runtime tag is shown correctly even if I removed all
    the env vars related to runtime.
    <img width="142" height="29" alt="image"
    src="https://github.com/user-attachments/assets/2e81c6d8-9695-4880-82fe-76561abd8e32"
    />
    
    
    # This PR
    Fix the problem by changing the path from `/etc` to `/etc/os-release`.
    
    Thanks @shreyamalpani for the idea about the fix.
    
    # Test
    ## Before
    Failed to parse the runtime
    <img width="489" height="34" alt="image"
    src="https://github.com/user-attachments/assets/ac1961f7-7565-4f82-9ab5-7b1f0ae48d07"
    />
    ## After
    Works for both al2 and al2023
    <img width="525" height="93" alt="image"
    src="https://github.com/user-attachments/assets/840db400-9995-439e-8ee0-0b819f8f76a2"
    />
    
    <img width="546" height="90" alt="image"
    src="https://github.com/user-attachments/assets/d24071dc-670b-4cc7-8fab-4d3fe6b222b0"
    />

--- bottlecap/src/proc/constants.rs
+++ bottlecap/src/proc/constants.rs
@@ -2,7 +2,7 @@ pub const PROC_NET_DEV_PATH: &str = "/proc/net/dev";
 pub const PROC_STAT_PATH: &str = "/proc/stat";
 pub const PROC_UPTIME_PATH: &str = "/proc/uptime";
 pub const PROC_PATH: &str = "/proc";
-pub const ETC_PATH: &str = "/etc";
+pub const ETC_PATH: &str = "/etc/os-release";
 
 pub const LAMBDA_NETWORK_INTERFACE: &str = "vinternal_1";
 pub const LAMBDA_RUNTIME_NETWORK_INTERFACE: &str = "vint_runtime";
