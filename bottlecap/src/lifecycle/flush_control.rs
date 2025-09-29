e0ec3bc85ff1358d644f3289eddda1ab1944dc1f
AJ Stuyvenberg <astuyve@gmail.com>
2025-09-29 11:02:31 -0400

    fix: setting last_flush to 0 makes us flush on the first invocation (#843)
    Setting the last flush time to 0 causes the logic for continuous
    flushing to always evaluate to `flush` on the first invocation.
    
    This change fixes that, unless there is a proactive initialization or
    some other out-of-band cause.

--- bottlecap/src/lifecycle/flush_control.rs
+++ bottlecap/src/lifecycle/flush_control.rs
@@ -27,9 +27,13 @@ pub enum FlushDecision {
 impl FlushControl {
     #[must_use]
     pub fn new(flush_strategy: FlushStrategy, flush_timeout: u64) -> FlushControl {
+        let now = time::SystemTime::now()
+            .duration_since(time::UNIX_EPOCH)
+            .expect("unable to poll clock, unrecoverable")
+            .as_secs();
         FlushControl {
             flush_strategy,
-            last_flush: 0,
+            last_flush: now,
             invocation_times: InvocationTimes::new(),
             flush_timeout,
         }
@@ -126,6 +130,8 @@ mod tests {
             FlushStrategy::EndPeriodically(PeriodicStrategy { interval: 1 }),
             60,
         );
+        // Set last_flush to an older time to trigger flush
+        flush_control.last_flush -= 2;
         assert_eq!(flush_control.evaluate_flush_decision(), FlushDecision::End);
 
         let mut flush_control = FlushControl::new(FlushStrategy::End, 60);
@@ -135,6 +141,8 @@ mod tests {
             FlushStrategy::Periodically(PeriodicStrategy { interval: 1 }),
             60,
         );
+        // Set last_flush to an older time to trigger flush
+        flush_control.last_flush -= 2;
         assert_eq!(
             flush_control.evaluate_flush_decision(),
             FlushDecision::Periodic
@@ -154,6 +162,8 @@ mod tests {
         for _ in 0..LOOKBACK_COUNT - 1 {
             assert_eq!(flush_control.evaluate_flush_decision(), FlushDecision::End);
         }
+        // Set last_flush to an older time to trigger flush after lookback threshold
+        flush_control.last_flush -= 61;
         assert_eq!(
             flush_control.evaluate_flush_decision(),
             FlushDecision::Continuous
@@ -166,6 +176,8 @@ mod tests {
             FlushStrategy::Periodically(PeriodicStrategy { interval: 1 }),
             60,
         );
+        // Set last_flush to an older time to trigger flush
+        flush_control.last_flush -= 2;
         assert_eq!(
             flush_control.evaluate_flush_decision(),
             FlushDecision::Periodic,
