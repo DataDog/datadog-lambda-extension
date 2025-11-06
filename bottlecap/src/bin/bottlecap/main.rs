4287f8a567949a3d55232b39a614c037e33be80c
John Chrostek <john.chrostek@datadoghq.com>
2025-11-06 13:06:02 -0500

    format

--- bottlecap/src/bin/bottlecap/main.rs
+++ bottlecap/src/bin/bottlecap/main.rs
@@ -866,7 +866,9 @@ async fn handle_event_bus_event(
                             error!("Failed to send platform restore report to processor: {}", e);
                         }
                     } else {
-                        error!("Missing SnapStart RestoreReportMetric. Not creating SnapStart span.")
+                        error!(
+                            "Missing SnapStart RestoreReportMetric. Not creating SnapStart span."
+                        )
                     }
                 }
                 TelemetryRecord::PlatformStart { request_id, .. } => {
