34d68aff88af9bb4a99bc9b4d7356f375ad49609
Tianning Li <tianning.li@datadoghq.com>
2026-01-13 15:10:47 -0500

    [integration-test]Add configurable wait time for OTLP integration tests (#994)
    https://datadoghq.atlassian.net/browse/SVLS-8286
    
    ## Overview
    We noticed OTLP integration tests were flaky. One speculation was we
    didn't wait long enough before retrieving the result.
    
    Hence we made Datadog indexing wait time configurable and use 5-minute
    wait for OTLP tests (instead of default 60s) to accommodate longer data
    propagation delays through OTLP collector pipeline.
    
    ## Testing 
    Local test:
    ```
    npm test -- otlp.test.ts
    ...
       Waiting 300s for logs and traces to be indexed in Datadog...
    ...
    Test Suites: 1 passed, 1 total
    Tests:       13 passed, 13 total
    Snapshots:   0 total
    Time:        318.945 s
    ```

--- integration-tests/tests/utils/util.ts
+++ integration-tests/tests/utils/util.ts
@@ -1,6 +1,12 @@
 import { DatadogLog, DatadogTrace, getTraces, getLogs } from "./datadog";
 import { invokeLambda } from "./lambda";
 
+// Default wait time for Datadog to index logs and traces after Lambda invocation
+const DEFAULT_DATADOG_INDEXING_WAIT_MS = 60 * 1000; // 60 seconds
+
+// Extended wait time for tests that need more time (e.g., OTLP tests)
+export const DATADOG_INDEXING_WAIT_5_MIN_MS = 5 * 60 * 1000; // 5 minutes
+
 export interface LambdaInvocationDatadogData {
     requestId: string;
     statusCode: number;
@@ -9,11 +15,17 @@ export interface LambdaInvocationDatadogData {
     logs?: DatadogLog[];
 }
 
-export async function invokeLambdaAndGetDatadogData(functionName: string, payload: any = {}, coldStart: boolean = false, useTailLogs: boolean = true): Promise<LambdaInvocationDatadogData> {
+export async function invokeLambdaAndGetDatadogData(
+    functionName: string,
+    payload: any = {},
+    coldStart: boolean = false,
+    useTailLogs: boolean = true,
+    datadogIndexingWaitMs: number = DEFAULT_DATADOG_INDEXING_WAIT_MS
+): Promise<LambdaInvocationDatadogData> {
     const result = await invokeLambda(functionName, payload, coldStart, useTailLogs);
 
-    console.log('Waiting for logs and traces to be indexed in Datadog...');
-    await new Promise(resolve => setTimeout(resolve, 60_000));
+    console.log(`Waiting ${datadogIndexingWaitMs / 1000}s for logs and traces to be indexed in Datadog...`);
+    await new Promise(resolve => setTimeout(resolve, datadogIndexingWaitMs));
 
     // Strip alias suffix (e.g., ":snapstart") for Datadog queries since service name doesn't include it
     const baseFunctionName = functionName.split(':')[0];
