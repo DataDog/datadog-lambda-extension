51d0f6d66e0dbc8b728e2b0174ccdeaf18ad125b
Pablo Martínez Bernardo <pablo.martinezbernardo@datadoghq.com>
2026-02-12 12:03:35 +0100

    extract_request_body before exiting handle_end_invocation

--- bottlecap/src/lifecycle/listener.rs
+++ bottlecap/src/lifecycle/listener.rs
@@ -168,16 +168,16 @@ impl Listener {
         State((invocation_processor_handle, _, tasks)): State<ListenerState>,
         request: Request,
     ) -> Response {
+        let (parts, body) = match extract_request_body(request).await {
+            Ok(r) => r,
+            Err(e) => {
+                error!("Failed to extract request body: {e}");
+                return (StatusCode::OK, json!({}).to_string()).into_response();
+            }
+        };
+
         let mut join_set = tasks.lock().await;
         join_set.spawn(async move {
-            let (parts, body) = match extract_request_body(request).await {
-                Ok(r) => r,
-                Err(e) => {
-                    error!("Failed to extract request body: {e}");
-                    return;
-                }
-            };
-
             Self::universal_instrumentation_end(&parts.headers, body, invocation_processor_handle)
                 .await;
         });
